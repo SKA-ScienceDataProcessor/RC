8e3e2e81b0f1ff89b9b5a5450bd0b4de7162b1de
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-06-18 19:05:16 +0400

    Additional combinators for folds

--- MS1/distributed-dot-product/RC/Combinators.hs
+++ MS1/distributed-dot-product/RC/Combinators.hs
@@ -3,8 +3,10 @@ module RC.Combinators (
     -- * Folds and loops
     foldMessages
   , foldMessagesMatch
+  , foldMatch
   , finiFoldMessages
   , finiFoldMessagesMatch
+  , finiFoldMatch
   , maybeLoop
   ) where
 
@@ -30,6 +32,13 @@ foldMessagesMatch m step
   where
     loop s = loop =<< step s =<< receiveWait m
 
+-- | Same as 'foldMessagesMatch' but update function is merged into 'Match'
+foldMatch :: [s -> Match s] -> s -> Process r
+foldMatch ms
+  = loop
+  where
+    loop s = loop =<< receiveWait [m s | m <- ms]
+
 -- | Fold received messages.
 finiFoldMessages
   :: (Serializable a) => (s -> a -> Process (Either b s)) -> s -> Process b
@@ -38,7 +47,7 @@ finiFoldMessages step
   where
     loop s = either return loop =<< step s =<< expect
 
--- | Same as 'foldMessages' but uses 'Match' for receiving messages
+-- | Same as 'finiFoldMessages' but uses 'Match' for receiving messages
 finiFoldMessagesMatch
   :: [Match a] -> (s -> a -> Process (Either b s)) -> s -> Process b
 finiFoldMessagesMatch m step
@@ -46,6 +55,14 @@ finiFoldMessagesMatch m step
   where
     loop s = either return loop =<< step s =<< receiveWait m
 
+
+-- | Same as 'foldMessages' but uses 'Match' for receiving messages
+finiFoldMatch
+  :: [s -> Match (Either a s)] -> s -> Process a
+finiFoldMatch ms
+  = loop
+  where
+    loop s = either return loop =<< receiveWait [m s | m <- ms]
 
 
 -- | Perform action on each received @Just@ value and terminate on
