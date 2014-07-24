326d906b5070be496bfd99804bd4bbe32bdb5bec
peterjbraam <peter.braam@peterbraam.com>
2014-07-24 13:15:29 +0900

    add CString parameter for path and offset

---
+++ MS1/assignment/d1/channel-file.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+/* XXX error handling: should we return an ssize_t
+   XXX types: buffer size is a ssize_t, not int
+*/
+
+void read_data(double *buf, int n, int o, char *p)
+{
+  int fd;
+
+  fd = open(p, O_RDONLY);
+  assert(fd > 0);
+  assert( pread(fd, buf, n * sizeof(double), o) == n * sizeof(double) );
+  return;
+}
