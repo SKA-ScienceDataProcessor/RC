96bc3fa40736e14fe04b1f57976266ddb73459c4
Serguey Zefirov <sergueyz@gmail.com>
2014-07-29 12:34:16 +0400

    Fix in cabal file and commit of the missing file.
    Cabal file now refers to DNA.Message in other-modules, and new Message.hs is committed.

--- MS1/ddp-erlang-style/DNA/Message.hs
+++ MS1/ddp-erlang-style/DNA/Message.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE TemplateHaskell, DeriveDataTypeable, DeriveGeneric #-}
 {-# OPTIONS_GHC -Wall #-}
-module DNA.Message (dnaSlaveHandleStart, dnaMasterStartSlave, DnaStart, DnaStarted, DnaFinished(DnaFinished), DnaPidList(DnaPidList)) where
+module DNA.Message (sayDebug, dnaSlaveHandleStart, dnaMasterStartSlave, DnaStart, DnaStarted, DnaFinished(DnaFinished), DnaPidList(DnaPidList)) where
 
 import Text.Printf
-import Control.Distributed.Process.Debug
+--import Control.Distributed.Process.Debug
 import Control.Distributed.Process
+import Control.Distributed.Process.Platform (resolve)
+import qualified Control.Distributed.Process.Platform.Service.SystemLog as Log
 import GHC.Generics (Generic)
 import Data.Binary
 import Data.Typeable
@@ -19,23 +21,42 @@ instance Binary DnaStarted
 instance Binary DnaFinished
 instance Binary DnaPidList
 
+sayDebug :: String -> Process ()
+sayDebug msg = do
+	logger <- Log.client
+	case logger of
+		Nothing -> do
+			--liftIO $ putStrLn $ "unable to resolve logger for "++show msg
+			log <- whereis "logger"
+			case log of
+				Just l -> do
+					us <- getSelfPid
+					send l $ ("<no time>", us, msg)
+				Nothing -> error "completely unable to resolve any logging facility!"
+			return ()
+		Just cl -> do
+			r <- resolve cl
+			say $ "cl agent "++show r
+			Log.debug cl (Log.LogText msg)
+
+
 -- in: name slavePid out: masterPid
 dnaSlaveHandleStart :: String -> ProcessId -> Process (ProcessId)
 dnaSlaveHandleStart name slavePid = do
-       	say $ printf "[%s] : Waiting for master." name
+       	sayDebug $ printf "[%s] : Waiting for master." name
 	(DnaStart masterPid) <- expect     
-  	say $ printf "[%s] : DnaStart from master :%s" name (show masterPid)
+  	sayDebug $ printf "[%s] : DnaStart from master :%s" name (show masterPid)
   	send masterPid (DnaStarted slavePid)
-  	say $ printf "[%s] : Started" name
+  	sayDebug $ printf "[%s] : Started" name
         return masterPid
 
 -- in: name masterPid nodeId  out: slavePid
 dnaMasterStartSlave :: String -> ProcessId -> NodeId -> Closure( Process() ) -> Process ProcessId
 dnaMasterStartSlave name masterPid nodeId clo = do
- 	say $ printf "[Master] : spawning %s on node: %s" name (show nodeId)
+ 	sayDebug $ printf "[Master] : spawning %s on node: %s" name (show nodeId)
         pid <- spawn nodeId clo
-        enableTrace pid
+--        enableTrace pid
         send pid (DnaStart masterPid)
         (DnaStarted slavePid) <- expect
- 	say $ printf "[Master] : %s started %s" name (show slavePid)
+ 	sayDebug $ printf "[Master] : %s started %s" name (show slavePid)
         return slavePid
