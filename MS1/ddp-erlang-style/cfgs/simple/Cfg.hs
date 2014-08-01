19b2171139e18ecfcf0e5b69a113931b915d5dc6
Serguey Zefirov <sergueyz@gmail.com>
2014-08-01 15:16:13 +0400

    Added missing files.

---
+++ MS1/ddp-erlang-style/cfgs/simple/Cfg.hs
@@ -0,0 +1,9 @@
+-- |Cfg.hs
+--
+-- Configuration module (executable names, build specific functions, etc).
+--
+-- Copyright (C) 2014 Braam Research
+
+module Cfg where
+
+executableName = "ddp-erlang-style"
