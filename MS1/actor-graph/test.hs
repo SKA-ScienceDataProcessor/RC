136b05c39504258d63eed7a3174bef0394b937ed
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-08-07 18:06:19 +0400

    Add program used for testing code generation

---
+++ MS1/actor-graph/test.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE GADTs #-}
+import DNA.Actor
+import DNA.AST
+import DNA.Compiler.CH
+import DNA.Compiler.Scheduler
+import DNA.Compiler.Types
+
+----------------------------------------------------------------
+-- Print actor
+----------------------------------------------------------------
+
+exprPrint :: Expr () (() -> Int -> ((),Out))
+exprPrint = Lam $ Lam $ Tup2 (Scalar ()) (Out [PrintInt $ Var ZeroIdx])
+
+actorPrint :: Actor ()
+actorPrint = actor $ do
+  rule $ exprPrint
+  startingState $ Scalar ()
+  return ()
+
+
+
+----------------------------------------------------------------
+-- Source actor
+----------------------------------------------------------------
+
+exprSrcInt :: ConnFree Int -> Expr () (Int -> (Int,Out))
+exprSrcInt i
+  = Lam $ Tup2 (Ap (Ap Add (Var ZeroIdx)) (Scalar (1 :: Int)))
+               (Out [Outbound i (Var ZeroIdx)])
+
+actorSrcInt :: Actor (ConnFree Int)
+actorSrcInt = actor $ do
+  c <- simpleOut
+  producer $ exprSrcInt c
+  startingState $ Scalar 0
+  return c
+
+
+program = do
+  (aPr, ()) <- use actorPrint
+  (_  , c ) <- use actorSrcInt
+  connect c aPr
+
+main :: IO ()
+main = do
+  let r = compile $ compileToCH =<< buildDataflow program
+  case r of
+    Left errs -> mapM_ putStrLn errs
+    Right prj -> saveProject "dir" prj
+
