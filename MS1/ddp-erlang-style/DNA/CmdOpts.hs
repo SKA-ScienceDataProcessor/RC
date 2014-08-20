5022fe04f994cc739400251500b11af089739b34
Serguey Zefirov <sergueyz@gmail.com>
2014-08-20 19:10:13 +0400

    Start of command-line parsing.

---
+++ MS1/ddp-erlang-style/DNA/CmdOpts.hs
@@ -0,0 +1,10 @@
+-- |CmdLine.hs
+--
+-- Command-line options parsing, using optparser-applicative.
+--
+-- Copyright (C) 2014 Braa Research, LLC.
+
+module DNA.CmdOpts (
+	  dnaParseCommandLineOpts
+	) where
+
