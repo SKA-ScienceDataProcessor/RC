3e5b9d6c492bb5519736ccf00cf00aa3858c1b14
Peter Braam <peter.braam@gmail.com>
2014-09-02 20:44:25 +0100

    add RTS options

--- MS1/ddp-erlang-style/test.sh
+++ MS1/ddp-erlang-style/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-
 #!/bin/sh
 cat >cad.example.txt <<EOF
 localhost:44440
@@ -14,8 +13,8 @@ EOF
 ln dist/build/ddp-erlang-style-SIMD-eventlog/ddp-erlang-style-SIMD-eventlog ddp-erlang-style
 ./create-floats INPUT 30 2 1
 ./create-floats INPUT 30 2 2
-./ddp-erlang-style slave --cad cad.example.txt --ip localhost --port 60001 &
-./ddp-erlang-style slave --cad cad.example.txt --ip localhost --port 60002 &
-./ddp-erlang-style slave --cad cad.example.txt --ip localhost --port 60003 &
+./ddp-erlang-style slave --cad cad.example.txt --ip localhost --port 60001 +RTS -l-au &
+./ddp-erlang-style slave --cad cad.example.txt --ip localhost --port 60002 +RTS -l-au &
+./ddp-erlang-style slave --cad cad.example.txt --ip localhost --port 60003 +RTS -l-au &
 sleep 1
-./ddp-erlang-style master --cad cad.example.txt --ip localhost --port 44440 --filename INPUT
+./ddp-erlang-style master --cad cad.example.txt --ip localhost --port 44440 --filename INPUT +RTS -l-au
