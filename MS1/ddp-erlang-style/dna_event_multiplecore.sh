ca0ead382c24c5f7bf2e4b510c79fb6d1b132896
Peter Braam <hpcbraam1@login-sand4.xcat.cluster>
2014-09-02 17:20:36 +0100

    Update test.sh and add slurm scripts

---
+++ MS1/ddp-erlang-style/dna_event_multiplecore.sh
@@ -0,0 +1,279 @@
+#!/bin/bash
+
+	
+if [ $# -eq 3 ]; then
+	callFunc=$1
+	rc=$2
+	if [ $callFunc == "start" ]; then
+		resource=$3
+	else
+		JOBID=$3
+	fi
+	
+elif [ $# -eq 4 ]; then
+	callFunc=$1
+	rc=$2
+	JOBID=$3
+	slurmJobNodeList=$4
+else
+	callFunc=usage
+	
+fi
+
+PWD=`pwd`
+binPath=$HOME/.cabal/bin
+Path=/usr/local/Cluster-Apps/slurm/bin
+getUser=`id -n -u`
+partition=`sacctmgr list user $getUser WithAssoc | awk '{print $6}' | tail -1`
+
+function start 
+{
+	submitJobToSLURM $resource $rc
+	if [ $? -eq 0 ]; then
+		echo -e "Job submission completed!!"
+	else
+		echo -e "Error : Some issue occurs while submitting!!"
+	fi
+
+
+
+}
+
+#Since, sbatch exits immediately after the script is successfully transferred to the SLURM controller and assigned a SLURM job ID. 
+#So, this function is required to keep the script alive untill all the execution completed.
+function submitJobToSLURM {
+
+	getResource=`echo $1`
+        echo -e "Job submission to SLURM initiated!!"
+        getJobID=`$Path/sbatch $PWD/$getResource $binPath/$rc | awk '{print $4}'`
+        while true;do
+                sleep 2
+
+                # Check job status
+                STATUS=`squeue -j $getJobID -t PD,R -h -o %t`
+
+                if [ "$STATUS" = "R" ];then
+                        # Job is running, break the while loop
+                        break
+                elif [ "$STATUS" != "PD" ];then
+                        echo "Job is not Running or Pending. Aborting"
+                        scancel $getJobID
+                        exit 1
+                fi
+
+                echo -n "."
+
+        done
+
+        while true;do
+                STATUS=`squeue -j $getJobID -t PD,R -h -o %t`
+                if [ "$STATUS" = "" ]; then
+                        break;
+                fi
+                echo -n "."
+        done
+
+        echo -e "Job submission to SLURM Completed and CH process!!\n"
+}
+
+function check {
+	slurmJobNodeList=$PWD/machine.file.$JOBID
+
+	if [ -f $slurmJobNodeList ]; then
+       		for machine in `cat $slurmJobNodeList`
+        	do
+                	for rcPid in `ssh $machine ps -ef | grep $rc | grep -v grep | grep -v slurmd | grep -v $callFunc |awk '{print $2}'`
+                	do
+				if [ "$rcPid" == "" ]; then
+					echo "Check operation performed on $machine. No binary running!!"
+				else
+					echo "Binary($rc) still running on $machine. Hence, exiting!!"
+					echo "Please execute - dna stop $rc $resource !!"
+					return 1
+				fi	
+                	done
+        	done
+	else
+		echo "Error : Either JOBID is wrong or machine.file.$JOBID has been removed!!"
+		return 1
+	fi
+}
+
+function stop {
+
+	slurmJobNodeList=$PWD/machine.file.$JOBID
+
+	if [ -f $slurmJobNodeList ]; then
+       		for machine in `cat $slurmJobNodeList`
+        	do
+                	for rcPid in `ssh $machine ps -ef | grep $rc | grep -v grep | grep -v slurmd | grep -v $callFunc | grep -v bin | awk '{print $2}'`
+                	do
+				if [ "$rcPid" == "" ]; then
+					echo "Stop operation performed. No binary running to kill!!"
+				else
+					echo "Killing binary ($rc) running on $machine!!"
+					kill $rcPid
+				fi	
+                	done
+        	done
+	else
+		echo "Error : Either JOBID is wrong or machine.file.$JOBID has been removed!!"
+		return 1
+	fi
+}
+
+#This function generated CAD list
+function generatingCADFile {
+        echo -e "Generating CAD File\n============\n"
+        touch $PWD/CAD.$JOBID.file
+
+	slurmJobNodeList=$PWD/machine.file.$JOBID	
+        portNo=79001 # Has some issue with automation, but can be done.
+        # Create a file in the format <Hostname>:<Port no>
+	for machine in `cat $slurmJobNodeList`
+        do
+		getIP=`ssh $machine nslookup $machine | grep Address | tail -1 | awk '{print $2}'` 
+                echo $getIP | sed "s/$/:${portNo}/" >> $PWD/CAD.$JOBID.file
+                portNo=`expr ${portNo} + 13`
+	done
+        echo `cat $PWD/CAD.$JOBID.file`
+	machineCount=`cat $slurmJobNodeList | wc -l`
+	countCAD=`cat $PWD/CAD.$JOBID.file | wc -l`
+	if [ $machineCount -eq $countCAD ]; then
+		echo "CAD file generated successfully!"
+	else
+		echo "CAD file is either incomplete or wrong information!!"
+		return 1
+	fi
+}
+
+function md5sum {
+        /usr/bin/md5sum $rc >  $PWD/md5sum.md5
+        status=`/usr/bin/md5sum -c $PWD/md5sum.md5`
+        if [ "$status" == "$rc: OK" ];then
+                return 0
+        else
+                return 1
+        fi
+}
+
+function startingCHprocess {
+set -x
+        echo -e "\n============\nCH Process initiated\n============\n"
+        flag=0
+	numberOfCores=12
+	noOfProcess=`cat $slurmJobNodeList | wc -l`
+	noOfComputeP=`expr $noOfProcess - 1`
+	#itemCount=`expr $noOfComputeP \* 15728640`
+	itemCounti=`expr $numberOfCores \* 150`
+	itemCount=`expr $itemCounti \* $noOfProcess`
+	linkCount=0
+	linkCountM=0
+        for machine in `cat $slurmJobNodeList`
+        do
+        	if [ $flag -eq 0 ]
+                then
+                        echo -e "Skip Master node IP\n============\n"
+                        getMasterIP=$machine
+                        flag=1
+                else
+                        ipAdd=`ssh $machine nslookup $machine | grep Address | tail -1 | awk '{print $2}'`
+                        getCount=`cat $PWD/CAD.$JOBID.file | grep $ipAdd | wc -l`
+			if [[ $getCount -gt 1 ]]; then
+                        	portNo=`cat $PWD/CAD.$JOBID.file | grep $ipAdd | head -1 |cut -f2 -d":"`
+			else	
+                        	portNo=`cat $PWD/CAD.$JOBID.file | grep $ipAdd | cut -f2 -d":"`
+			fi
+                        echo -e "\n============\nStarting Slave==<$ipAdd>=====<$portNo>=====\n============\n"
+			cp $rc slave.$machine
+			coreCount=1	
+			chunkSize=0
+			UDPPortNo=$portNo 
+			if [[ $linkCount -eq 0 ]];then
+				srun -p $partition --nodelist $machine -N1 -n1 --exclusive ln -s /ramdisks/file.$machine $HOME/input_data.txt
+				linkCount=1
+			fi
+			while [ $coreCount -le $numberOfCores ]; 
+			do
+				chunkSize=`expr $chunkSize + 1`
+				srun -p $partition --nodelist $machine -N1 -n1 --exclusive ./create-floats /ramdisks/file.$machine $itemCount 72 $chunkSize
+                        	srun -p $partition --nodelist $machine -N1 -n1 --exclusive ./slave.$machine slave --cad $PWD/CAD.$JOBID.file --ip $ipAdd --port $UDPPortNo +RTS -l-au & 
+                      #  	srun -p $partition --nodelist $machine --resv-ports -N1 -n1 --exclusive ./slave.$machine slave $ipAdd $UDPPortNo +RTS -l-au -N12 & 
+				UDPPortNo=`expr $UDPPortNo + 1`
+				coreCount=`expr $coreCount + 1`
+			done
+                	sleep 2
+               		echo -e "Sleeping for 2 secs!!"
+                fi
+        done
+
+        ipAdd=`nslookup $getMasterIP | grep Address | tail -1 | awk '{print $2}'`
+        getCount=`cat $PWD/CAD.$JOBID.file | grep $ipAdd | wc -l`
+	if [[ $getCount -gt 1 ]]; then
+        	portNo=`cat $PWD/CAD.$JOBID.file | grep $ipAdd | head -1 | cut -f2 -d":"`
+	else
+        	portNo=`cat $PWD/CAD.$JOBID.file | grep $ipAdd | cut -f2 -d":"`
+	fi
+	
+        echo -e "\n============\nStarting Master====<$ipAdd>=====<$portNo>=====\n============\n"
+	cp $rc master.$getMasterIP	
+	coreCount=1	
+	UDPPortNo=$portNo 
+	srun -p $partition --nodelist $getMasterIP -N1 -n1 --exclusive rm -rf $HOME/input_data.txt
+	srun -p $partition --nodelist $getMasterIP -N1 -n1 --exclusive touch /ramdisks/file.$getMasterIP
+	srun -p $partition --nodelist $getMasterIP -N1 -n1 --exclusive ln -s /ramdisks/file.$getMasterIP $HOME/input_data.txt
+	while [ $coreCount -le $numberOfCores ]; 
+	do
+		srun -p $partition --nodelist $getMasterIP -N1 -n1 --exclusive ./create-floats /ramdisks/file.$getMasterIP $itemCount 72 0 
+       		srun -p $partition --nodelist $getMasterIP -N1 -n1 --exclusive ./master.$getMasterIP master --cad $PWD/CAD.$JOBID.file --ip $ipAdd --port $UDPPortNo --filename $HOME/input_data.txt +RTS -l-au 
+		UDPPortNo=`expr $UDPPortNo + 1`
+                coreCount=`expr $coreCount + 1`
+	done
+        #srun -p $partition --nodelist $getMasterIP -N1 -n1 --exclusive ./master.$getMasterIP master $ipAdd $portNo input_data.txt norm +RTS -lsgpfu 
+
+        echo -e "\n============\nCH Process initiation completed\n============\n"
+}
+
+function collectLogs {
+	echo -e "\n============\nCollecting all the logs\n============\n"
+	mkdir -p $PWD/Logs_$JOBID
+
+	flag=0
+	for machine in `cat $slurmJobNodeList`
+	do
+		if [ $flag -eq 0 ]
+                then
+			ghc-events show $PWD/master.$machine.eventlog > $PWD/Logs_$JOBID/master.$machine.eventlog.txt
+			mv $PWD/master.$machine.eventlog $PWD/Logs_$JOBID/.
+			rm $PWD/master.$machine
+			flag=1
+		else
+			ghc-events show $PWD/slave.$machine.eventlog > $PWD/Logs_$JOBID/slave.$machine.eventlog.txt
+			mv $PWD/slave.$machine.eventlog $PWD/Logs_$JOBID/.
+			rm $PWD/slave.$machine
+		fi
+		#srun -p $partition --nodelist $machine -N1 -n1 --exclusive rm -rf $HOME/input_data.txt
+		srun -p $partition --nodelist $machine -N1 -n1 --exclusive rm -rf /ramdisks/file.$machine 
+	done		
+	mv $PWD/slurm-$JOBID.out $PWD/Logs_$JOBID/.
+	mv $PWD/machine.file.$JOBID $PWD/Logs_$JOBID/.
+	mv $PWD/CAD.$JOBID.file $PWD/Logs_$JOBID/.
+	mv $PWD/md5sum.md5 $PWD/Logs_$JOBID/.
+
+	echo -e "\n============\nCollecting of logs completed\n============\n"
+	
+	
+}
+
+####### Main
+
+function usage
+{
+	echo -e "Usage: "
+	echo -e "./dna.sh start <dot-product binary> <requested SLURM resource script>"
+	echo -e "./dna.sh check <dot-product binary> <JOBID>"
+	echo -e "./dna.sh stop <dot-product binary> <JOBID>"
+	
+}
+
+$callFunc 
---
+++ MS1/ddp-erlang-style/resource_event_multiplecore.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+rc=$1
+
+#################################################################
+# Please note that you need to adapt this script to your job
+# Submitting it as is will fail. 
+##################################################################
+# Define the job name
+#SBATCH -J dot-product 
+#
+#SBATCH --partition=tesla
+# Advised: your Email here, for job notification
+#SBATCH --mail-user=jaya@braamresearch.com
+#SBATCH --mail-type=ALL
+#     (ALL = BEGIN, END, FAIL, REQUEUE)
+#
+# Set a pattern for the output file.
+#SBATCH --output=slurm-%j.out
+#SBATCH --profile=ALL
+#SBATCH --acctg-freq=network=infiniband/ofed
+#  By default both standard output and  standard  error are 
+# directed to a file of the name "slurm-%j.out", where the "%j" 
+# is replaced with the job allocation number.   The filename 
+# pattern may contain one or more replacement symbols, which are 
+# a percent sign "%" followed by a letter (e.g. %j).
+#
+# Supported replacement symbols are:
+#     %j     Job allocation number.
+#     %N     Main node name.  
+#
+##################################################################
+# The requested run-time
+#
+#SBATCH --time=00:10:00
+#SBATCH --exclusive
+# Acceptable time formats include "minutes", "minutes:seconds", 
+# "hours:minutes:seconds", "days-hours", "days-hours:minutes" 
+# and "days-hours:minutes:seconds"
+#
+# Slurm will kill your job after the requested period.
+# The default time limit is the partition’s time limit.
+#
+# Note that the lower the requested run-time, the higher the
+# chances to get scheduled to 'fill in the gaps' between other
+# jobs. 
+#
+##################################################################
+# Requested number of cores. Choose either of, or both of
+#
+####SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#
+# Set a to be the number of process you want to launch and b the
+# number of threads per process. Typically, for an MPI job with 8 
+# processes, set a=8, b=1. For an OpenMP job with 12 threads, set
+# a=1, b=12. For a hybrid 8-process, 12-threads per process MPI 
+# job with OpenMP 'inside', set a=8, b=12.
+# 
+# You can also set 
+#SBATCH --nodes=6
+#SBATCH --ntasks-per-node=12
+#SBATCH --qos=gpu0
+# 
+# to force your jobs to run at most at c at a time on a single 
+# node. The --exclusive option reservers the whole node for your
+# job. Remove one '#' before them to activate. 
+#
+##################################################################
+# Requested memory for each core
+#
+#SBATCH --mem-per-cpu=1024
+#
+# Set the memory requirements for the job in MB. Your job will be
+# allocated exclusive access to that amount of RAM. In the case it
+# overuses that amount, Slurm will kill it. The default value is 
+# around 2GB per core.
+#
+# Note that the lower the requested memory, the higher the
+# chances to get scheduled to 'fill in the gaps' between other
+# jobs. 
+#
+##################################################################
+ 
+# end of job
+
+##################################################################
+SLURM_LOCALID=1
+PWD=`pwd`
+
+echo "Current directory: `pwd`"
+JOBID=$SLURM_JOB_ID
+
+echo -e "JobID: $JOBID and ntasks :$SLURM_NTASKS_PER_NODE\n======"
+echo "Time: `date`"
+echo "Running on master node: `hostname`"
+
+if [ "$SLURM_JOB_NODELIST" ]; then
+        #! Create a machine file:
+        export NODEFILE=`generate_pbs_nodefile`
+        cat $NODEFILE | uniq > $PWD/machine.file.$JOBID
+        echo -e "\nNodes allocated:\n================"
+        echo `cat $PWD/machine.file.$JOBID | sed -e 's/\..*$//g'`
+fi
+
+slurmJobNodeList=$PWD/machine.file.$JOBID
+
+$PWD/dna_event_multiplecore.sh check $rc $JOBID $slurmJobNodeList
+if [ $? -eq 0 ]; then
+	echo -e "dna check performed!!"
+else
+	echo -e "Some issue occured in dna check !!"
+	scancel $JOBID
+	exit
+fi
+
+$PWD/dna_event_multiplecore.sh generatingCADFile $rc $JOBID $slurmJobNodeList 
+if [ $? -eq 0 ]; then
+	echo -e "generatingCADFile performed!!"
+else
+	echo -e "Some issue occured in generatingCADFile !!"
+	scancel $JOBID
+	exit
+fi
+
+$PWD/dna_event_multiplecore.sh md5sum $rc 0 0 
+if [ $? -eq 0 ]; then
+	echo -e "md5sum performed!!"
+else
+	echo -e "Some issue occured in md5sum !!"
+	scancel $JOBID
+	exit
+fi
+
+$PWD/dna_event_multiplecore.sh startingCHprocess $rc $JOBID machine.file.$JOBID 
+if [ $? -eq 0 ]; then
+	echo -e "startingCHprocess performed!!"
+else
+	echo -e "Some issue occured in startingCHprocess !!"
+	scancel $JOBID
+	exit
+fi
+
+$PWD/dna_event_multiplecore.sh collectLogs 0 $JOBID $slurmJobNodeList
+if [ $? -eq 0 ]; then
+	echo -e "collectLogs performed!!"
+else
+	echo -e "Some issue occured in collectLogs !!"
+	exit
+fi
+
+wait
+
