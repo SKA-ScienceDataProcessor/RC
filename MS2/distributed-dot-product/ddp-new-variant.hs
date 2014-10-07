1e602518c66b2016d816801551fced0cafffe8b1
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-10-07 23:02:28 +0400

    First varaint of shorter programs.
    Which compiles but of course does not work

--- MS2/distributed-dot-product/ddp-new-variant.hs
+++ MS2/distributed-dot-product/ddp-new-variant.hs
@@ -8,6 +8,7 @@ import Control.Monad
 import Control.Distributed.Process hiding (say)
 import Control.Distributed.Process.Closure
 import qualified Control.Distributed.Process.Platform.UnsafePrimitives as Unsafe
+import Control.Distributed.Process.Serializable (Serializable)
 
 import Data.Binary   (Binary)
 import Data.Int
@@ -29,6 +30,9 @@ import DNA.Run
 newtype Master = Master ProcessId
                  deriving (Show,Eq,Typeable,Binary)
 
+newtype Param a = Param a
+                 deriving (Show,Eq,Typeable,Binary)
+
 -- | Newtype for encoding order of function parameters.  It's possible
 --   to rely on order of sending but it's easy to mix up and not
 --   reliable in hypothetical case when we send parameters from
@@ -45,8 +49,21 @@ newtype S a = S a
               deriving (Show,Eq,Typeable,Binary)
 
 
--- | Type synonym to make explicit type signatures shorter
-type MakeChan a = Process (SendPort a, ReceivePort a)
+data Promise a = Promise (ReceivePort a)
+
+-- | Obtain value from promise. This function should be called only once per promise
+promise :: Promise a -> Process a
+promise (Promise ch) = do
+    -- Here we wait for either value from channel or for message that
+    -- child died.
+    receiveWait
+        [ matchChan ch return
+        , match $ \(ProcessMonitorNotification _ _ reason) ->
+            case reason of
+              DiedNormal -> promise (Promise ch)
+              _          -> terminate
+        ]
+
 
 -- | Cluster architecture description. Currently it's simply list of
 --   nodes process can use.
@@ -65,6 +82,43 @@ scatterShape n size
     chunkOffs    = scanl (+) 0 chunkSizes
 
 
+-- | Start process
+startProcess :: (Serializable a, Serializable b)
+             => ([NodeId] -> a -> Process b)
+             -> Process ()   
+startProcess action = do
+    sendCh  <- expect
+    nodes   <- expect
+    Param a <- expect
+    b       <- action nodes a
+    sendChan sendCh b
+
+-- | Fork process on local node
+forkLocal :: (Serializable a, Serializable b)
+          => [NodeId]           -- ^ List of nodes process allowed to use
+          -> Process ()         -- ^ Process command
+          -> a                  -- ^ Parameters to process
+          -> Process (Promise b)
+forkLocal nodes child a = do
+    undefined
+
+
+-- | Fork process on remote node
+forkRemote :: (Serializable a, Serializable b)
+           => [NodeId]             -- ^ List of nodes process allowed to use
+           -> NodeId               -- ^ Node to spawn on
+           -> Closure (Process ()) -- ^ Sub process command
+           -> a                    -- ^ Parameters sent to process
+           -> Process (Promise b)
+forkRemote nodes nid child a = do
+    (pid,_) <- spawnSupervised nid child
+    (chSend,chRecv) <- newChan
+    send pid chSend
+    send pid (CAD nodes)
+    send pid (Param a)
+    return $ Promise chRecv
+
+
 
 ----------------------------------------------------------------
 -- Distributed dot product
@@ -75,57 +129,23 @@ scatterShape n size
 
 -- | Compute vector and send it back to master using unsafe send.
 ddpComputeVector :: Process ()
-ddpComputeVector = do
-  result    <- expect :: Process (SendPort (S.Vector Double)) 
-  S (off,n) <- expect :: Process (S (Int,Int))
-  -- Generate vector.
-  --
-  -- NOTE: we need strictness here otherwise we will pass unevaluated
-  --       thunk to parent process
-  let !vec = S.generate n (\i -> fromIntegral (i + off)) :: S.Vector Double
-  Unsafe.sendChan result vec
-
+ddpComputeVector = startProcess $ \_ (off,n) ->
+    return $ (S.generate n (\i -> fromIntegral (i + off)) :: S.Vector Double)
 
 -- | Read vector slice from the data file.
 ddpReadVector :: Process ()
-ddpReadVector = do
-  result        <- expect :: Process (SendPort (S.Vector Double))
-  S fname       <- expect :: Process (S FilePath)
-  S (S (off,n)) <- expect :: Process (S (S (Int64,Int64)))
-  --
-  me   <- getSelfPid
-  !vec <- liftIO $ readDataMMap n off fname "FIXME"
-  Unsafe.sendChan result vec
+ddpReadVector = startProcess $ \_ (fname, (off,n)) -> do
+    liftIO $ readDataMMap n off fname "FIXME"
 
 
 -- | Caclculate dot product of slice of vector
 ddpProductSlice :: Process ()
-ddpProductSlice = do
-  -- Receive PID of master
-  me            <- getSelfPid
-  Master parent <- expect
-  -- Function parameters
-  S fname       <- expect :: Process (S FilePath)
-  S (S (off,n)) <- expect :: Process (S (S (Int64,Int64)))
-  -- Spawn local workers for generating vectors
-  (computeSend,computeRecv) <- newChan :: MakeChan (S.Vector Double)
-  computePID <- spawnLocal ddpComputeVector
-  send computePID computeSend
-  send computePID (S (off,n))
-  --
-  (fileSend,fileRecv) <- newChan :: MakeChan (S.Vector Double)
-  filePID <- spawnLocal ddpReadVector
-  send filePID fileSend
-  send filePID (S fname)
-  send filePID (S (S (off,n)))
-  -- This is synchronization point. We need vector from both helper
-  -- actors to proceed.
-  va <- receiveChan computeRecv
-  vb <- receiveChan fileRecv
-  let r = S.sum $ S.zipWith (*) va vb
-  -- Send partial result to the parent
-  send parent r
-
+ddpProductSlice = startProcess $ \_ (fname, slice) -> do
+    futVA <- forkLocal [] ddpComputeVector slice
+    futVB <- forkLocal [] ddpReadVector (fname :: String, slice :: (Int64,Int64))
+    va <- promise futVA
+    vb <- promise futVB
+    return $ (S.sum $ S.zipWith (*) va vb :: Double)
 
 
 remotable [ 'ddpComputeVector
