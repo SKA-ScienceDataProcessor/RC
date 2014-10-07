758e701f7e4a45787f43720db914181dd32cf35b
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-10-07 15:17:27 +0400

    Remove different Cfg and add frist variant of Logging routines
    Logging is due to overhaul anyway and wthi will simplify build

--- MS2/distributed-dot-product/DNA/Logging.hs
+++ MS2/distributed-dot-product/DNA/Logging.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 -- | Logging.hs
 --
 -- Logging facilities.
@@ -5,4 +6,16 @@
 -- Copyright (C) 2014 Braam Research, LLC.
 module DNA.Logging where
 
--- FIXME. nothing there yet.
+
+-- | Dictionary of logging functions. Note that logger could implement
+--   only parts of described functionality.
+data Logger m = Logger
+  { -- | Measure how long action will execute and put it into eventlog.
+    timePeriod :: forall a. String -> m a -> m a
+  , -- | Measure how long does pure computation take to compute and put it into eventlog.
+    timePeriodPure :: forall a. String -> a -> a
+  , -- | Synchronize timings - put into eventlog an event with current wall time.
+    synchronizationPoint :: m ()
+  , -- | Put message to event log
+    eventMessage :: String -> m ()
+  }
