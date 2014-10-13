440ab37c1f109d02d11c2ce3b37d52984d23bb62
Serguey Zefirov <sergueyz@gmail.com>
2014-10-13 16:06:20 +0400

    Makefile add and gridding code fixes
    Makefile expects Halide next to root of RC git repository. If RC is in /home/user/work/RC, then put Halide to directory /home/user/halide
    
    Make builds gridding executable which, when run, should produce C++ code.
    
    Right now Halide complains about non-pure definition of G. I am on the search for problematic part of code.

--- MS2/halide-gridding/gridding.cpp
+++ MS2/halide-gridding/gridding.cpp
@@ -1,24 +1,30 @@
 // Gridding algoithm, adopted from bilateral_grid demo app.
 // There's nothing left from bilateral_grid except ideas and typical Halide idioms.
 
+#include <math.h>
 #include "Halide.h"
 #include <stdio.h>
 
 using namespace Halide;
 
 int main(int argc, char **argv) {
+
+    double pi_const = acos(-1);
+    Expr pi("pi");
+    pi = cast<double>(pi_const);
+
     ImageParam UVW(Float(64), 4);    // second dimension is used for U, V and W parts. So it should be Nx3 sized.
 
     // Constant supersample factor.
     Expr supersampleFactor = 8;
 
     // The Omega - telescope view angle.
-    Expr Omega = pi/2;
+    Expr Omega = cast<double>(pi/2);
 
     // Two dimensional image for prolate spheroidal wave function. Last dimension is fixed to 2.
     ImageParam PSWF(Float(64),3);
 
-    Var pswfKernelHalf("pswfKernelHalf");
+    Expr pswfKernelHalf("pswfKernelHalf");
 
     // pswf kernel image height and width are odd, 2N+1. To recover the half, and not to overflow image bounds,
     // we first subtract 1 and only then divide by 2. It have to work for 2N+1 and is safe for even values.
@@ -28,39 +34,39 @@ int main(int argc, char **argv) {
     // Iterator within UVW triples.
     RDom uvwRange (0, UVW.extent(0), 0, UVW.extent(1), 0, UVW.extent(2));
 
-    Var timestep("timestep");
-    Var baseline("baseline");
-    Var channel("channel");
+    Expr timestep("timestep");
+    Expr baseline("baseline");
+    Expr channel("channel");
 
     baseline = uvwRange.x;
-    timestep = uvmRange.y;
+    timestep = uvwRange.y;
     timestep = uvwRange.z;
 
     // fetch the values.
-    Var U("U");
+    Expr U("U");
     U = UVW(baseline, timestep, channel, 0);
-    Var V("V");
+    Expr V("V");
     V = UVW(baseline, timestep, channel, 1);
-    Var W("W");
+    Expr W("W");
     W = UVW(baseline, timestep, channel, 2);
 
-    Expr intU = cast<int>U;
-    Expr intV = case<int>V;
+    Expr intU = cast<int>(U);
+    Expr intV = cast<int>(V);
 
     Expr supersampleU = cast<int>(supersampleFactor*(U - intU));
     Expr supersampleV = cast<int>(supersampleFactor*(V - intV));
 
     // Computation of kernel width.
-    Expr onePercent = 0.01;
+    Expr onePercent = cast<double>(0.01);
     Expr wOmegaBy2 = W*Omega/2;
-    Expr GKernelWidth = sqrt(wOmegaBy2*(wOmegaBy2 + pi*onePercent/sqrt(w)));
-    Expr GKernelWidthInt = cast<int>(GKernelWidth+0.5);
+    Expr GKernelWidth = sqrt(wOmegaBy2*(wOmegaBy2 + cast<double>(pi)*cast<double>(onePercent)/sqrt(W)));
+    Expr GKernelWidthInt = cast<int>(GKernelWidth+cast<double>(0.5));
 
     // Range over kernel space. It is two dimensional, to account for U and V.
     RDom GUVRange (-GKernelWidthInt, GKernelWidthInt+1, -GKernelWidthInt, GKernelWidthInt+1);
 
-    Var GU("GU");
-    Var GV("GV");
+    Expr GU("GU");
+    Expr GV("GV");
 
     GU = GUVRange.x;
     GV = GUVRange.y;
@@ -68,8 +74,8 @@ int main(int argc, char **argv) {
     // Range over PSWF space.
     RDom pswfRange(-pswfKernelHalf, pswfKernelHalf+1, -pswfKernelHalf, pswfKernelHalf+1);
 
-    Var PSWFU("PSWFU");
-    Var PSWFV("PSWFV");
+    Expr PSWFU("PSWFU");
+    Expr PSWFV("PSWFV");
 
     PSWFU = pswfRange.x;
     PSWFV = pswfRange.y;
@@ -78,12 +84,12 @@ int main(int argc, char **argv) {
     Func G("G");
 
     // !!! XXX FIXME !!! XXX FIXME
-    G(GU,GV) = 1-G*0.01-U*0.01;
+    G(GU,GV) = 1-GU*0.01-GV*0.01;
 
     // The computation of the result.
     Func gridding("gridding");
-    var rU("rU");
-    Var rV("rV");
+    Expr rU("rU");
+    Expr rV("rV");
 
     // computing the result
     rU = intU + GU+PSWFU;
