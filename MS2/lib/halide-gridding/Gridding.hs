40a81cf8c973cf3079116309162c0a4abea4a297
Serguey Zefirov <sergueyz@gmail.com>
2014-11-04 03:31:02 +0300

    Removed polymorphism
    I will make everything work with floats, then go to other cases.

--- MS2/lib/halide-gridding/Gridding.hs
+++ MS2/lib/halide-gridding/Gridding.hs
@@ -25,10 +25,10 @@ import System.IO
 import System.IO.Error
 
 -- |Opaque data type to type pointers to Halide images.
-data HalideImage a
+data HalideImage
 
 -- |The pointer itself.
-type HalideImagePtr a = Ptr (HalideImage a)
+type HalideFloatImagePtr = Ptr HalideImage
 
 
 data GriddingMode = GriddingSimple | GriddingInterpolate | GriddingOversample | GriddingCompute
@@ -37,25 +37,25 @@ data GriddingMode = GriddingSimple | GriddingInterpolate | GriddingOversample | 
 -- |Gridding input. Opaque to the user.
 data GriddingInput = GriddingInput {
           griddingInputMode             :: GriddingMode -- defines presence and structure of supports.
-        , griddingInputUVW              :: HalideImagePtr Float
-        , griddingInputVisibilities     :: HalideImagePtr Float
-        , griddingSupports              :: Maybe (HalideImage Float)
+        , griddingInputUVW              :: HalideFloatImagePtr
+        , griddingInputVisibilities     :: HalideFloatImagePtr
+        , griddingSupports              :: Maybe HalideFloatImagePtr
         }
 
 -- |Allocating 1D image.
-foreign import ccall "halideFloatImage1D" halideFloatImage1D :: Int -> IO (HalideImagePtr Float)
+foreign import ccall "halideFloatImage1D" halideFloatImage1D :: Int -> IO HalideFloatImagePtr
 
 -- |Allocating 2D image.
-foreign import ccall "halideFloatImage2D" halideFloatImage2D :: Int -> Int -> IO (HalideImagePtr Float)
+foreign import ccall "halideFloatImage2D" halideFloatImage2D :: Int -> Int -> IO HalideFloatImagePtr
 
 -- |Allocating 3D image.
-foreign import ccall "halideFloatImage3D" halideFloatImage3D :: Int -> Int -> Int -> IO (HalideImagePtr Float)
+foreign import ccall "halideFloatImage3D" halideFloatImage3D :: Int -> Int -> Int -> IO HalideFloatImagePtr
 
 -- |Allocating 4D image.
-foreign import ccall "halideFloatImage4D" halideFloatImage4D :: Int -> Int -> Int -> Int -> IO (HalideImagePtr Float)
+foreign import ccall "halideFloatImage4D" halideFloatImage4D :: Int -> Int -> Int -> Int -> IO HalideFloatImagePtr
 
 -- |Get the size of Halide image, in bytes.
-foreign import ccall "halideFloatImageDataSize" halideFloatImageDataSize :: HalideImagePtr Float -> IO Int64
+foreign import ccall "halideFloatImageDataSize" halideFloatImageDataSize :: HalideFloatImagePtr -> IO Int64
 
 -- |Read Int64 from file.
 readInt64 :: Handle -> IO Int64
@@ -64,15 +64,14 @@ readInt64 h = do
         return $ runGet get bytes
 
 -- |Seeking the beginning of 4096 page.
-seekPageBegin :: [Int64] -> Handle -> IO ()
-seekPageBegin chunksSizes handle = do
-        let     toPage x = div (x + 4095) 4096
-        let     chunksPages = map toPage chunksSizes
-        let     page = sum chunksPages + 1
+seekPageBegin :: Int64 -> Handle -> IO ()
+seekPageBegin chunksPages handle = do
+        let     page = chunksPages + 1
         hSeek handle AbsoluteSeek (fromIntegral page * 4096)
 
-readImageContent :: [Int64] -> Handle -> HalideImagePtr Float -> IO [Int64]
+readImageContent :: Int64 -> Handle -> HalideFloatImagePtr -> IO Int64
 readImageContent sizes handle image = do
+        
         error "read image!!!"
 
 
@@ -86,8 +85,8 @@ readGriddingData mode filePath = flip catchIOError (const $ return Nothing) $ do
                 timesteps <- readInt64 h
                 uvwImage <- halideFloatImage3D (fromIntegral baselines) (fromIntegral timesteps) 3
                 visibilities <- halideFloatImage3D (fromIntegral baselines) (fromIntegral timesteps) 8
-                sizes <- readImageContent [] h uvwImage
-                readImageContent sizes h visibilities
+                pagesRead <- readImageContent 0 h uvwImage
+                readImageContent pagesRead h visibilities
                 return $ Just $ GriddingInput {
                           griddingInputMode = mode
                         , griddingInputUVW = uvwImage
