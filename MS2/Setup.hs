85da8be00f684b39f6074da5c878edbd8d2a2c84
Serguey Zefirov <sergueyz@gmail.com>
2014-11-09 07:28:54 +0300

    Accomodation of Halide bindings
    I started to make sure dna-programs run fine, including Halide gridding implementation.

--- MS2/Setup.hs
+++ MS2/Setup.hs
@@ -1,2 +1,7 @@
 import Distribution.Simple
-main = defaultMain
+
+-- |Change preconfigure hooks - build Halide objects, etc.
+dnaProgramsUserHooks = simpleUserHooks  -- record update coming soon.
+
+-- |We slightly change behaviour of cabal pipeline to accomodate for Halide.
+main = defaultMainWithHooks dnaProgramsUserHooks
