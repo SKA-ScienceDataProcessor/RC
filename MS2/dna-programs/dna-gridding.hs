93ee14f876abca47691f62dc7e43d8087a171367
Serguey Zefirov <sergueyz@gmail.com>
2014-11-09 07:09:40 +0300

    Add of dna-gridding test program
    Added dna-gridding .hs source file and executable entry into cabal file.

---
+++ MS2/dna-programs/dna-gridding.hs
@@ -0,0 +1,13 @@
+-- |dna-gridding.hs
+--
+-- Grdding performance and conformance tests, both GPU and CPU.
+--
+-- Copyright (C) 2014 Braam Research, LLC.
+
+module Main where
+
+
+
+main :: IO ()
+main = do
+    return ()
