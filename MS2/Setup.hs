0d8ac1683a84b305848f2d6ddf4335b16c006855
Serguey Zefirov <sergueyz@gmail.com>
2014-11-11 18:35:32 +0300

    Building dna-test-programs in custom way
    We have to check for Halide when building dna-programs. To do this properly we have to execute IO actions to determine directories and other environment intricacies.
    
    I commented out one of executables because it gives build errors. Will restore it shortly.

--- MS2/Setup.hs
+++ MS2/Setup.hs
@@ -1,7 +1,36 @@
+-- |Setup.hs
+--
+-- A custom build script for dna programs.
+--
+-- Copyright (C) 2014 Braam Research, LLC.
+
+module Main(main) where
+
 import Distribution.Simple
+import Distribution.Simple.Configure (configure)
+import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
+import Distribution.PackageDescription
 
 -- |Change preconfigure hooks - build Halide objects, etc.
-dnaProgramsUserHooks = simpleUserHooks  -- record update coming soon.
+dnaProgramsUserHooks = simpleUserHooks {
+          confHook = preConfHalideConfigureBuild
+        }
+
+-- |Check for Halide and update configuration for building C++ files.
+preConfHalideConfigureBuild :: (GenericPackageDescription, HookedBuildInfo)
+            -> ConfigFlags -> IO LocalBuildInfo
+preConfHalideConfigureBuild (packageDescr, buildInfo) configFlags = do
+        localBuildInfo <- configure (packageDescr, buildInfo) configFlags
+{-
+        putStrLn $ unlines
+                [ show packageDescr
+                , show buildInfo
+                , show configFlags
+                , show localBuildInfo
+                ]
+-}
+        return localBuildInfo
 
 -- |We slightly change behaviour of cabal pipeline to accomodate for Halide.
 main = defaultMainWithHooks dnaProgramsUserHooks
