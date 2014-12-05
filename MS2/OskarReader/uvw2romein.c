6806c6e0f92b1c6d5691207793c7759949d8035f
Kyrill Briantsev <kyrab@mail.ru>
2014-12-05 16:41:40 +0300

    Transposition (unoptimized) to H/R compatible layout.

--- MS2/OskarReader/uvw2romein.c
+++ MS2/OskarReader/uvw2romein.c
@@ -3,7 +3,7 @@
   Example of o2a library usage.
 
   Reads and reshuffles u, v, w and amp data to the
-  layout, suitable for Romein and Halide gridders consumption.
+  layout, suitable for Romein (R) and Halide (H) gridders consumption.
 
   Copyright (C) 2014 Braam Research, LLC.
  */
@@ -16,7 +16,8 @@ int main(int argc, char** argv){
   VisHandle vh;
   Dimensions dims;
   DoubleData data;
-  int i, n;
+  int i, j, n;
+  int BL, bloff;
   FILE * out;
   const double *cu, *cv, *cw;
 
@@ -36,7 +37,7 @@ int main(int argc, char** argv){
       printf("Writing u v w ...\n");
       out = fopen("uvw.dat", "wb");
 
-      // No fancy optimizations. Plain stupid loop.
+      #ifdef __OSKAR_NATIVE_ORDER
       n = dims.timesteps * dims.baselines;
       for ( i = 0
           , cu = data.u
@@ -49,6 +50,24 @@ int main(int argc, char** argv){
         fwrite(cv, sizeof (*cv), 1, out);
         fwrite(cw, sizeof (*cw), 1, out);
       }
+      #else
+      // To be compatible with H/R shall swap
+      // baselines and timesteps dimensions.
+      // No fancy optimizations. Plain stupid loops.
+      cu = data.u;
+      cv = data.v;
+      cw = data.w;
+      BL = dims.baselines;
+      for ( i = 0; i < BL; i++ ) {
+        for ( j = 0; j < dims.timesteps; j++ ) {
+          bloff = BL * j + i;
+          fwrite(cu + bloff, sizeof (*cu), 1, out);
+          fwrite(cv + bloff, sizeof (*cv), 1, out);
+          fwrite(cw + bloff, sizeof (*cw), 1, out);
+        }
+      }
+      #endif
+
       fclose(out);
     } else {
       printf("Can't allocate datastructures.\n");
