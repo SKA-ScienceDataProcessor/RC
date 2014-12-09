215c73857b3546aa68faa9a14af40fef930d34d0
Kyrill Briantsev <kyrab@mail.ru>
2014-12-09 13:41:34 +0300

    Correct declarations. Simplify.

--- MS2/lib/halide-gridding/dbl/GriddersFFI.hs
+++ MS2/lib/halide-gridding/dbl/GriddersFFI.hs
@@ -3,6 +3,7 @@
   , UnliftedFFITypes
   , UnboxedTuples
   , GHCForeignImportPrim
+  , GeneralizedNewtypeDeriving
   #-}
 
 module GriddersFFI where
@@ -10,18 +11,18 @@ module GriddersFFI where
 import GHC.Ptr (Ptr(..), FunPtr(..))
 import GHC.Types(IO(..), Int(I#))
 import GHC.Prim (Addr#, State#, RealWorld, Int#)
+import Foreign.C.Types (CDouble)
 
 data Grid
 type GridHandle = Ptr Grid
 
-newtype Status = St Int deriving Show
+newtype Status = St Int deriving (Num, Show)
 
-type GridFinalizer = GridHandle -> IO ()
+type GridFinalizer = Addr# -> IO ()
 
 data GridData = GD {
-    gdHandle    :: GridHandle
-  , gdData      :: Ptr Double
-  , gdFinalizer :: GridFinalizer
+    gdData      :: Ptr CDouble
+  , gdFinalizer :: IO ()
   }
 
 foreign import ccall "dynamic" 
@@ -32,22 +33,22 @@ type CGPrim = Addr# -> Addr# -> State# RealWorld -> (# State# RealWorld, Addr#, 
 foreign import prim "romeinComputeGridOnCudazh" romeinComputeGridOnCuda# :: CGPrim
 foreign import prim "halideComputeGridOnCudazh" halideComputeGridOnCuda# :: CGPrim
 
-romeinComputeGridOnCuda :: Ptr Double -> Ptr Double -> IO (Either Status GridData)
+romeinComputeGridOnCuda :: Ptr CDouble -> Ptr CDouble -> IO (Either Status GridData)
 romeinComputeGridOnCuda (Ptr uvwp) (Ptr ampp) = IO doit
   where
     doit s0 = case romeinComputeGridOnCuda# uvwp ampp s0 of
                 (# s1, hdl, data_addr, fin, status #) ->
                    let res = case status of
-                               0# -> Right (GD (Ptr hdl) (Ptr data_addr) (mkGridFinalizer $ FunPtr fin))
+                               0# -> Right (GD (Ptr data_addr) (mkGridFinalizer (FunPtr fin) hdl))
                                _  -> Left (St (I# status))
                    in (# s1, res #)
 
-halideComputeGridOnCuda :: Ptr Double -> Ptr Double -> IO (Either Status GridData)
+halideComputeGridOnCuda :: Ptr CDouble -> Ptr CDouble -> IO (Either Status GridData)
 halideComputeGridOnCuda (Ptr uvwp) (Ptr ampp) = IO doit
   where
     doit s0 = case halideComputeGridOnCuda# uvwp ampp s0 of
                 (# s1, hdl, data_addr, fin, status #) ->
                    let res = case status of
-                               0# -> Right (GD (Ptr hdl) (Ptr data_addr) (mkGridFinalizer $ FunPtr fin))
+                               0# -> Right (GD (Ptr data_addr) (mkGridFinalizer (FunPtr fin) hdl))
                                _  -> Left (St (I# status))
                    in (# s1, res #)
