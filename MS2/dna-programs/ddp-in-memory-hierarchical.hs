fad17b61b9830c6f9972a8f2a259fa6cc3e72f25
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2014-12-28 13:18:50 +0300

    And once again I forgot to add file to version control

---
+++ MS2/dna-programs/ddp-in-memory-hierarchical.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE BangPatterns #-}
+module Main(main) where
+
+import Data.Int
+import qualified Data.Vector.Storable as S
+
+import DNA
+
+import DDP
+import DDP_Slice
+
+
+----------------------------------------------------------------
+-- Distributed dot product
+--
+-- Note that actors which do not spawn actors on other nodes do not
+-- receive CAD.
+----------------------------------------------------------------
+
+-- | Actor for calculating dot product
+ddpDotProduct :: Actor (String,Slice) Double
+ddpDotProduct = actor $ \(fname, size) -> do
+    -- Chunk local product
+    rnk   <- rank
+    gSize <- groupSize
+    let slice = scatterSlice gSize size !! rnk
+    -- Chunk & send out
+    res   <- selectMany (Frac 1) (NNodes 1) [UseLocal]
+    shell <- startGroup res $(mkStaticClosure 'ddpProductSlice)
+    broadcastParam (fname,slice) shell
+    partials <- delayGroup shell
+    x <- duration "collecting vectors" $ gather partials (+) 0
+    return x
+
+remotable [ 'ddpDotProduct
+          ]
+
+ddpDotProductMaster :: Actor (String,Slice) Double
+ddpDotProductMaster = actor $ \(fname,size) -> do
+    res   <- selectMany (Frac 1) (NWorkers 3) [UseLocal]
+    shell <- startGroup res $(mkStaticClosure 'ddpDotProduct)
+    broadcastParam (fname,size) shell
+    partials <- delayGroup shell
+    x <- duration "collection partials" $ gather partials (+) 0
+    return x
+
+
+main :: IO ()
+main = do
+    dnaRun rtable $ do
+        b <- eval ddpDotProductMaster ("file.dat", Slice 0 20000000)
+        liftIO $ putStrLn $ "RESULT: " ++ show b
+  where
+    rtable = DDP.__remoteTable
+           . DDP_Slice.__remoteTable  
+           . Main.__remoteTable
