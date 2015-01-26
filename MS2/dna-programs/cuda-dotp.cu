4853ea7634435f51697b654ca321b95881cbde58
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2015-01-26 18:37:08 +0300

    Check for errors while starting CUDA kernel

--- MS2/dna-programs/cuda-dotp.cu
+++ MS2/dna-programs/cuda-dotp.cu
@@ -1,4 +1,5 @@
 
+#include <stdio.h>
 /* Global variable for returning result of computation */
 __device__ float g_res;
 
@@ -10,6 +11,14 @@ __global__ void dotp_kernel(double xs[], double ys[]) {
     double mine = xs[idx] * ys[idx];
     atomicAdd(&g_res, mine);
 }
+
+inline void gpuAssert(cudaError_t err, const char* file, int line) {
+    if( err != cudaSuccess ) {
+        fprintf(stderr,"GPU error: %s %s %d\n", cudaGetErrorString(err), file, line);
+        exit(1);
+    }
+}
+#define GPU_ASSERT(ans) {gpuAssert(ans,__FILE__,__LINE__);}
 
 /* Execute kernel on GPU */
 extern "C"
@@ -24,16 +33,16 @@ double calculate_dot_p(double xs[], double ys[], int n) {
     double *buf_xs = NULL;
     double *buf_ys = NULL;
     // Allocate memory and copy to device
-    cudaMalloc( &buf_xs, n*sizeof(double) );
-    cudaMalloc( &buf_ys, n*sizeof(double) );
-    cudaMemcpy(buf_xs, xs, n*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(buf_ys, ys, n*sizeof(double), cudaMemcpyHostToDevice);
+    GPU_ASSERT( cudaMalloc( &buf_xs, n*sizeof(double) ) );
+    GPU_ASSERT( cudaMalloc( &buf_ys, n*sizeof(double) ) );
+    GPU_ASSERT( cudaMemcpy(buf_xs, xs, n*sizeof(double), cudaMemcpyHostToDevice) );
+    GPU_ASSERT( cudaMemcpy(buf_ys, ys, n*sizeof(double), cudaMemcpyHostToDevice) );
     // Execute kernel
     dotp_kernel<<<grid, block>>>(buf_xs, buf_ys);
     // Free buffers & obtain data
     cudaFree( &buf_xs );
     cudaFree( &buf_ys );
     double res;
-    cudaMemcpyFromSymbol(&res, g_res, sizeof(double), 0, cudaMemcpyDeviceToHost);
+    GPU_ASSERT( cudaMemcpyFromSymbol(&res, g_res, sizeof(double), 0, cudaMemcpyDeviceToHost) );
     return res;
 }
