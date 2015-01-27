7f9c6cb9177b8cf28afa2f77c2e1d359d168e2d6
Kyrill Briantsev <kyrab@mail.ru>
2015-01-27 16:27:49 +0300

    Halide replaced with another (fake double atomics) variant of Romein. The main program is rewritten.

--- MS2/lib/DSL_Gridder/gridding-in-memory.hs
+++ MS2/lib/DSL_Gridder/gridding-in-memory.hs
@@ -10,7 +10,7 @@
 
 module Main(main) where
 
-import Text.Printf (printf)
+-- import Text.Printf (printf)
 import Control.Distributed.Static (Closure)
 
 import GriddersFFI(
@@ -30,9 +30,9 @@ import qualified DGridding as DG
 mkCfg :: IO (FilePath, FilePath)
 mkCfg =
   let
-   (os, sky_model, vis_name, ini_name) = mk_ska1low_test_cfg "ska1low.sky" FREQ_START 1 72 1800 "../../../../uvwsim/telescope"
+   (os, sky_model, vis_name, ini_name) = mk_ska1low_test_cfg "ska1sur.sky" FREQ_START 1 72 1800 "../../../../../uvwsim/tele_sur"
   in do
-       writeFile "ska1low.sky" sky_model
+       writeFile "ska1sur.sky" sky_model
        writeFile ini_name (showSettings os)
        return (vis_name, ini_name)
 
@@ -53,32 +53,52 @@ remotable [ 'gridActor
 doThemAll :: Actor () String
 doThemAll = actor $ \_ -> do
     (vis_file_name, ini_name) <- liftIO mkCfg
+    let rawsys = $(mkStaticClosure 'rawSystemActor)
+    --
+    rgcf <- select Remote (N 1)
+    gcf <- startActor rgcf rawsys
+    sendParam ("GCF", []) gcf
+    gcfwaiter <- delay Remote gcf
+    --
     retcode <- eval rawSystemActor ("oskar_sim_interferometer", [ini_name])
     case retcode of
       Just err -> return $ "oskar_sim_interferometer failed with: " ++ show err
-      Nothing -> do
+      _ -> do
         cvtstatus <- eval convertVisActor vis_file_name
         if cvtstatus /= 0
           then return $ "uvwv convertor failed with: " ++ show cvtstatus
           else do
-                let
-                   fnames = (uvw_filename vis_file_name, amp_filename vis_file_name)
-                   grid_closure = $(mkStaticClosure 'gridActor)
-                --
-                rgridder1 <- select Remote (N 1)
-                gridder1  <- startActor rgridder1 grid_closure
-                waiter_gridder1 <- delay Remote gridder1
-                --
-                rgridder2 <- select Remote (N 1)
-                gridder2  <- startActor rgridder2 grid_closure
-                waiter_gridder2 <- delay Remote gridder2
-                --
-                sendParam ("Romein", $(mkStaticClosure 'romeinActor), fnames) gridder1
-                sendParam ("Halide", $(mkStaticClosure 'halideActor), fnames) gridder2
-                -- Ignore them for now. Writer should spit some diagnostics.
-                (_rcode1,msg1) <- await waiter_gridder1
-                (_rcode2,msg2) <- await waiter_gridder2
-                return $ printf "Finished with:\n\t%s from the 1st writer,\n\ts from the 2nd writer" msg1 msg2
+            let
+               fnames = (uvw_filename vis_file_name, amp_filename vis_file_name)
+               grid_closure = $(mkStaticClosure 'gridActor)
+            --
+            rgridder1 <- select Remote (N 1)
+            gridder1  <- startActor rgridder1 grid_closure
+            waiter_gridder1 <- delay Remote gridder1
+            --
+            rgridder2 <- select Remote (N 1)
+            gridder2  <- startActor rgridder2 grid_closure
+            waiter_gridder2 <- delay Remote gridder2
+            --
+            sendParam ("Romein_f", $(mkStaticClosure 'romeinActor_f), fnames) gridder1
+            sendParam ("Romein", $(mkStaticClosure 'romeinActor), fnames) gridder2
+            -- Ignore them for now. Writer should spit some diagnostics.
+            (_rcode1,_msg1) <- await waiter_gridder1
+            (_rcode2,_msg2) <- await waiter_gridder2
+            --
+            gcfret <- await gcfwaiter
+            case gcfret of
+              Just err -> return $ "GCF creation failed with: " ++ show err
+              _ -> do
+                     binres <- eval rawSystemActor ("binner", [vis_file_name])
+                     case binres of
+                       Just err -> return $ "binner failed with: " ++ show err
+                       _ -> do
+                         bgridderres <- eval rawSystemActor ("RunGridder1", [])
+                         case bgridderres of
+                           Just err -> return $ "Simple CUDA gridder failed with: " ++ show err
+                           _ -> return "All is good and well!!!"
+
 
 main :: IO ()
 main = dnaRun (DG.__remoteTable . __remoteTable) $ eval doThemAll () >>= liftIO . putStrLn
