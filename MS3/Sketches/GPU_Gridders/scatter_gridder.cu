dfc3a174a14ebd8044dad14c7c8b8581a56511bd
Kyrill Briantsev <kyrab@mail.ru>
2015-03-16 17:05:06 +0300

    Reduce code duplication slightly.

--- MS3/Sketches/GPU_Gridders/scatter_gridder.cu
+++ MS3/Sketches/GPU_Gridders/scatter_gridder.cu
@@ -1,36 +1,5 @@
 #include "scatter_gridder.h"
-
-__device__ double atomicAdd(double* address, double val)
-{
-#ifdef FAKE_ATOMIC
-  double orig = *address;
-  *address = orig + val;
-  return orig;
-#else
-  unsigned long long int* address_as_ull = (unsigned long long int*)address;
-  unsigned long long int old = *address_as_ull, assumed;
-  do {
-       assumed = old;
-       old = atomicCAS(address_as_ull, assumed,
-                         __double_as_longlong(val +
-                           __longlong_as_double(assumed)));
-       // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
-  } while (assumed != old);
-  return __longlong_as_double(old);
-#endif
-}
-
-__device__ __inline__ void atomicAdd(Double4c *ptr, complexd sumXX, complexd sumXY, complexd sumYX, complexd sumYY)
-{
-  atomicAdd(&ptr->XX.x, sumXX.x);
-  atomicAdd(&ptr->XX.y, sumXX.y);
-  atomicAdd(&ptr->XY.x, sumXY.x);
-  atomicAdd(&ptr->XY.y, sumXY.y);
-  atomicAdd(&ptr->YX.x, sumYX.x);
-  atomicAdd(&ptr->YX.y, sumYX.y);
-  atomicAdd(&ptr->YY.x, sumYY.x);
-  atomicAdd(&ptr->YY.y, sumYY.y);
-}
+#include "atomic_add.h"
 
 struct Pregridded
 {
