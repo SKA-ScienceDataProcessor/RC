b27776313d4b603001c8b9a9a6a5360be33cce8f
Kyrill Briantsev <kyrab@mail.ru>
2015-03-17 19:47:23 +0300

    More analysis.

--- MS3/Sketches/Oskar_New_Reader/wplane_info.cpp
+++ MS3/Sketches/Oskar_New_Reader/wplane_info.cpp
@@ -1,38 +1,50 @@
 /* 
   Copyright (C) 2015 Braam Research, LLC.
  */
-
 #include <cstdio>
+#include <cstring>
+
+#define WPLANES 32
+#define TIMESTEPS 200
+#define BASELINES 32131
+
+int slices[BASELINES];
 
 struct baseline_descr {
   int bl;
   int items;
 };
 
-int main(int argc, char * argv[]) {
+void proc(int wplane) {
+  static char name[64];
+  sprintf(name, "wplanes/%02d_wplane.dat", wplane);
 
-  if (argc < 2) {
-    printf("usage: %s <wplane file>\n", argv[0]);
-  }
+  FILE * f = fopen(name, "rb");
+  if (f == nullptr) return;
 
-  FILE * f = fopen(argv[1], "rb");
-  if (f == nullptr) {
-    printf("Error opening %s\n", argv[1]);
-    return -1;
-  }
-
+  printf("At %02d wplane:\n", wplane);
   fseek(f, -sizeof(int), SEEK_END);
   int num_baselines;
   fread(&num_baselines, sizeof(int), 1, f);
-  printf("File %s contains info for %d baselines:\n", argv[1], num_baselines);
+  printf("File %s contains info for %d baselines:\n", name, num_baselines);
   fseek(f, -(sizeof(int) + sizeof(baseline_descr) * num_baselines), SEEK_END);
 
   for(int i=0; i < num_baselines; i++) {
     baseline_descr bld;
     fread(&bld, sizeof(baseline_descr), 1, f);
     const char * tmark;
-    if (bld.items != 200) tmark = " TRANS";
+    if (bld.items != TIMESTEPS) tmark = " TRANS";
     else tmark = "";
     printf("%8d %8d%s\n", bld.bl, bld.items, tmark);
+    slices[bld.bl]++;
+  }
+}
+
+int main(){
+  for(int i=0; i < BASELINES; i++) slices[i] = 0;
+  for(int i=0; i < WPLANES; i++) proc(i);
+  for(int i=0; i < BASELINES; i++) {
+    if (slices[i] > 1)
+      printf("Baseline %2d is sliced between %d planes\n", i, slices[i]);
   }
 }
