9329aea5fc93324b08b1900abaf883797fe2169f
Kyrill Briantsev <kyrab@mail.ru>
2015-03-18 12:19:54 +0300

    Even more analysis.

--- MS3/Sketches/Oskar_New_Reader/wplane_info.cpp
+++ MS3/Sketches/Oskar_New_Reader/wplane_info.cpp
@@ -3,12 +3,18 @@
  */
 #include <cstdio>
 #include <cstring>
+#include <utility>
+
+using namespace std;
 
 #define WPLANES 32
 #define TIMESTEPS 200
 #define BASELINES 32131
 
-int slices[BASELINES];
+// Very quick and dirty.
+// We already know we have no more than
+//   2 slices per baseline.
+pair<int,int> slices[BASELINES][2];
 
 struct baseline_descr {
   int bl;
@@ -36,15 +42,32 @@ void proc(int wplane) {
     if (bld.items != TIMESTEPS) tmark = " TRANS";
     else tmark = "";
     printf("%8d %8d%s\n", bld.bl, bld.items, tmark);
-    slices[bld.bl]++;
+    if (slices[bld.bl][0].second == 0) {
+      slices[bld.bl][0].first = wplane;
+      slices[bld.bl][0].second = bld.items;
+    }
+    else {
+      slices[bld.bl][1].first = wplane;
+      slices[bld.bl][1].second = bld.items;
+    }
   }
 }
 
 int main(){
-  for(int i=0; i < BASELINES; i++) slices[i] = 0;
-  for(int i=0; i < WPLANES; i++) proc(i);
   for(int i=0; i < BASELINES; i++) {
-    if (slices[i] > 1)
-      printf("Baseline %2d is sliced between %d planes\n", i, slices[i]);
+    slices[i][0] = {0, 0};
+    slices[i][1] = {0, 0};
   }
+  for(int i=0; i < WPLANES; i++) proc(i);
+  int sliced = 0;
+  for(int i=0; i < BASELINES; i++) {
+    if (slices[i][1].second != 0) {
+      sliced++;
+      printf("Baseline %2d is sliced between 2 planes thus: %2d[%2d] %2d[%2d]\n", i
+        , slices[i][0].first, slices[i][0].second
+        , slices[i][1].first, slices[i][1].second
+        );
+    }
+  }
+  printf("%d sliced baselines total.\n", sliced);
 }
