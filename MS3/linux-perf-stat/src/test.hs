5af7f48aee79bb792e349a7660d1c484de77ba41
Peter Wortmann <Peter.Wortmann@braamresearch.com>
2015-03-19 12:32:55 +0100

    Test linux-perf-stat for dot product implemented in Haskell, SSE and AVX.
    This is actually important - we can verify that we can indeed
    catch both SSE as well as AVX SIMD operations. We especially
    get an idea of how much double-counting we can expect. Bad news
    here is that it turns out to be substantial (~60%) for naive
    SSE/AVX. Proper prefetching pushes this number below 10%.

--- MS3/linux-perf-stat/src/test.hs
+++ MS3/linux-perf-stat/src/test.hs
@@ -4,13 +4,59 @@
 module Main where
 
 import Control.Monad
+
 import Profiling.Linux.Perf.Stat
 import Data.Time.Clock
 import Text.Printf
 
--- This is inspired by:
---
--- http://icl.cs.utk.edu/projects/papi/wiki/PAPITopics:SandyFlops
+import qualified Data.Vector.Storable as V
+
+import Foreign.Ptr
+import Foreign.C.Types
+
+import System.IO.Unsafe
+
+foreign import ccall unsafe "sse_ddp"
+  c_sse_ddp :: Ptr CFloat -> Ptr CFloat -> CInt -> IO Float
+foreign import ccall unsafe "sse_ddp_d"
+  c_sse_ddp_d :: Ptr CFloat -> Ptr CFloat -> CInt -> IO Double
+
+foreign import ccall unsafe "avx_ddp"
+  c_avx_ddp :: Ptr CFloat -> Ptr CFloat -> CInt -> IO Float
+foreign import ccall unsafe "avx_ddp_d"
+  c_avx_ddp_d :: Ptr CFloat -> Ptr CFloat -> CInt -> IO Double
+
+sseDDP :: V.Vector Float -> V.Vector Float -> Float
+sseDDP in1 in2 =
+  unsafePerformIO $
+  V.unsafeWith in1 $ \pin1 ->
+  V.unsafeWith in2 $ \pin2 -> do
+    let cnt = V.length in1
+    c_sse_ddp (castPtr pin1) (castPtr pin2) (fromIntegral cnt)
+
+sseDDPd :: V.Vector Double -> V.Vector Double -> Double
+sseDDPd in1 in2 =
+  unsafePerformIO $
+  V.unsafeWith in1 $ \pin1 ->
+  V.unsafeWith in2 $ \pin2 -> do
+    let cnt = V.length in1
+    c_sse_ddp_d (castPtr pin1) (castPtr pin2) (fromIntegral cnt)
+
+avxDDP :: V.Vector Float -> V.Vector Float -> Float
+avxDDP in1 in2 =
+  unsafePerformIO $
+  V.unsafeWith in1 $ \pin1 ->
+  V.unsafeWith in2 $ \pin2 -> do
+    let cnt = V.length in1
+    c_avx_ddp (castPtr pin1) (castPtr pin2) (fromIntegral cnt)
+
+avxDDPd :: V.Vector Double -> V.Vector Double -> Double
+avxDDPd in1 in2 =
+  unsafePerformIO $
+  V.unsafeWith in1 $ \pin1 ->
+  V.unsafeWith in2 $ \pin2 -> do
+    let cnt = V.length in1
+    c_avx_ddp_d (castPtr pin1) (castPtr pin2) (fromIntegral cnt)
 
 main :: IO ()
 main = do
@@ -22,49 +68,73 @@ main = do
            , PfmDesc "FP_COMP_OPS_EXE:X87"
            , PfmDesc "FP_COMP_OPS_EXE:SSE_FP_SCALAR_SINGLE"
            , PfmDesc "FP_COMP_OPS_EXE:SSE_SCALAR_DOUBLE"
-           --, PfmDesc "FP_COMP_OPS_EXE:SSE_PACKED_SINGLE"
-           --, PfmDesc "FP_COMP_OPS_EXE:SSE_FP_PACKED_DOUBLE"
-           --, PfmDesc "SIMD_FP_256:PACKED_SINGLE"
+           , PfmDesc "FP_COMP_OPS_EXE:SSE_PACKED_SINGLE"
+           , PfmDesc "FP_COMP_OPS_EXE:SSE_FP_PACKED_DOUBLE"
+           , PfmDesc "SIMD_FP_256:PACKED_SINGLE"
+           , PfmDesc "SIMD_FP_256:PACKED_DOUBLE"
 #endif
            ]
 
-  let go_f :: Int -> Float -> Float
-      go_f !0 !c = c
-      go_f !a !c = go_f (a-1) (c + fromIntegral a * fromIntegral a)
-      test_f = go_f 100000 0 `seq` return ()
+  let size = 1000 * 1000 * 10
+      in1f, in2f :: V.Vector Float
+      !in1f = V.replicate size 10
+      !in2f = V.generate size (\i -> 0.1 * fromIntegral i)
+      in1d, in2d :: V.Vector Double
+      !in1d = V.replicate size 10
+      !in2d = V.generate size (\i -> 0.1 * fromIntegral i)
 
-      go_d :: Int -> Double -> Double
-      go_d !0 !c = c
-      go_d !a !c = go_d (a-1) (c + fromIntegral a * fromIntegral a)
-      test_d = go_d 100000 0 `seq` return ()
+      expected :: RealFloat a => a
+      expected = fromIntegral size * fromIntegral size / 2
+      check :: (Show a, RealFloat a) => a -> IO ()
+      check v =
+        when (abs (v - expected) > expected / 10) $
+          putStrLn $ "Wrong result: " ++ show v ++ " != " ++ show (expected :: Double)
 
-  -- Warm up
-  replicateM_ 10 $ test_d >> test_f
+  let tests = [ check $ V.sum $ V.zipWith (*) in1f in2f
+              , check $ V.sum $ V.zipWith (*) in1d in2d
+              , check $ sseDDP in1f in2f
+              , check $ sseDDPd in1d in2d
+              , check $ avxDDP in1f in2f
+              , check $ avxDDPd in1d in2d
+              ]
 
-  forM_ [test_f, test_d] $ \test -> do
+  -- Warm up, and prevent full laziness
+  replicateM_ 2 $ sequence tests
+
+  forM_ tests $ \test -> do
 
     -- Run loop
+    let repeats = 10
     beginTime <- getCurrentTime
     begin <- perfEventRead group
-    test
+    replicateM_ repeats $ test
     end <- perfEventRead group
     endTime <- getCurrentTime
+    let expectedOps = size * 2 * repeats
 
     -- Show stats
-    let (cycles:instrs:x87:scalarSingle:scalarDouble:_)
+    let (cycles:instrs:x87:
+         scalarSingle:scalarDouble:
+         packedSingle:packedDouble:
+         avx256Single:avx256Double:_)
              = zipWith perfEventDiff end begin
         time = toRational $ endTime `diffUTCTime` beginTime
         perTime :: PerfStat -> Double
         perTime ps = 1000 * fromIntegral (psValue ps) / fromIntegral (psTimeRunning ps)
     putStrLn $ unlines $ map concat
-      [ [ printf "Time:          %6d ms" (floor (time * 1000000) :: Int) ]
-      , [ printf "Time Running:  %6d ms" (psTimeRunning cycles `div` 1000) ]
-      , [ printf "Time Enabled:  %6d ms" (psTimeEnabled cycles `div` 1000) ]
-      , [ printf "Cycles:        %6d     - %8.2f MHz"   (psValue cycles) (perTime cycles) ]
-      , [ printf "Instructions:  %6d     - %8.2f MHz"   (psValue instrs) (perTime instrs) ]
-      , [ printf "x87:           %6d OPs - %8.2f MOP/s" (psValue x87)    (perTime x87)    ]
-      , [ printf "Scalar Single: %6d OPs - %8.2f MOP/s" (psValue scalarSingle) (perTime scalarSingle) ]
-      , [ printf "Scalar Double: %6d OPs - %8.2f MOP/s" (psValue scalarDouble) (perTime scalarDouble) ]
+      [ [ printf "Time:          %10d ms" (floor (time * 1000000) :: Int) ]
+      , [ printf "Time Running:  %10d ms" (psTimeRunning cycles `div` 1000) ]
+      , [ printf "Time Enabled:  %10d ms" (psTimeEnabled cycles `div` 1000) ]
+      , [ printf "Cycles:        %10d     - %8.2f MHz"   (psValue cycles) (perTime cycles) ]
+      , [ printf "Instructions:  %10d     - %8.2f MHz"   (psValue instrs) (perTime instrs) ]
+      , [ printf "x87:           %10d OPs - %8.2f MOP/s" (psValue x87)    (perTime x87)    ]
+      , [ printf "Scalar Single: %10d OPs - %8.2f MOP/s" (psValue scalarSingle) (perTime scalarSingle) ]
+      , [ printf "Scalar Double: %10d OPs - %8.2f MOP/s" (psValue scalarDouble) (perTime scalarDouble) ]
+      , [ printf "SSE Single:    %10d OPs - %8.2f MOP/s" (4*psValue packedSingle) (4*perTime packedSingle) ]
+      , [ printf "SSE Double:    %10d OPs - %8.2f MOP/s" (2*psValue packedDouble) (2*perTime packedDouble) ]
+      , [ printf "AVX Single:    %10d OPs - %8.2f MOP/s" (8*psValue avx256Single) (8*perTime avx256Single) ]
+      , [ printf "AVX Double:    %10d OPs - %8.2f MOP/s" (4*psValue avx256Double) (4*perTime avx256Double) ]
+      , [ printf "Reference:     %10d OPs - %8.2f MOP/s" (expectedOps) (fromRational (fromIntegral expectedOps / time / 1000000) :: Float) ]
       ]
 
   perfEventClose group
