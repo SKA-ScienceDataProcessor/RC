e08b104019bdac7e76a415d4852ba6ef7c812e92
Kyrill Briantsev <kyrab@mail.ru>
2015-03-26 17:43:08 +0300

    CUDA global variable doesn't seem working.

--- MS3/Sketches/GCF_new/all.cu
+++ MS3/Sketches/GCF_new/all.cu
@@ -3,14 +3,13 @@
 
 #include "threadFenceReduction_kernel.cuh"
 
-__device__ double norm;
-
-extern "C" __global__ void reduce_512_odd(const cuDoubleComplex *g_idata, unsigned int n) {
-  reduceSinglePass_dev<512, false>(g_idata, &norm, n);
+extern "C" __global__ void reduce_512_odd(const cuDoubleComplex *g_idata, double *g_odata, unsigned int n) {
+  reduceSinglePass_dev<512, false>(g_idata, g_odata, n);
 }
 
 extern "C" __global__ void normalize(
-    cuDoubleComplex v[257*257]
+    double * normp
+  , cuDoubleComplex v[257*257]
   ){
-  normalize_kernel<128>(norm, v);
+  normalize_kernel<128>(1.0 / (*normp), v);
 }
