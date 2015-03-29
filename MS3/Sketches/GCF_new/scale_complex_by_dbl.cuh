05253c3157bf1c009c7c42c52897020c2f1b9aa0
Kyrill Briantsev <kyrab@mail.ru>
2015-03-29 18:22:57 +0300

    Forgot added file for previous commit: 'Slightly generalize ...'

---
+++ MS3/Sketches/GCF_new/scale_complex_by_dbl.cuh
@@ -0,0 +1,25 @@
+#ifndef __SCALE_COMPLEX_BY_DBL_CUH
+#define __SCALE_COMPLEX_BY_DBL_CUH
+
+#include <cuda.h>
+#include <cuComplex.h>
+#include <math_constants.h>
+
+__device__ static __inline__ cuDoubleComplex cuMulComplexByDouble(cuDoubleComplex v,
+                                                             double y){
+  return make_cuDoubleComplex ( v.x * y
+                              , v.y * y
+                              );
+}
+
+__device__ __inline__
+void scale_complex_by_dbl(
+    double norm
+  , cuDoubleComplex * v
+  , int len
+  ) {
+  const int x = blockIdx.x * blockDim.x + threadIdx.x;
+  if (x < len) v[x] = cuMulComplexByDouble(v[x], norm);
+}
+
+#endif
