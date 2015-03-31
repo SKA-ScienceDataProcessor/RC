b18a9f9f33749e47262bb928ab91ba536ba470c4
Kyrill Briantsev <kyrab@mail.ru>
2015-03-31 19:42:32 +0300

    Factored out gcf module.

--- MS3/Sketches/GCF_new/GCF.hs
+++ MS3/Sketches/GCF_new/GCF.hs
@@ -5,16 +5,16 @@
     , TypeSynonymInstances
   #-}
 
-module Main where
+module GCF (
+    createGCF
+  , finalizeGCF
+  , GCF(..)
+  ) where
 
 import Data.Int
 import Foreign.Storable
 import Foreign.Storable.Complex ()
-import Foreign.Ptr
 -- import Foreign.Marshal.Alloc -- for DEBUG only!
-import Data.Time.Clock
-import qualified Data.ByteString.Unsafe      as BS
-import qualified Data.ByteString             as BS
 -- import Text.Printf(printf)
 import qualified Foreign.CUDA.Driver as CUDA
 -- import qualified Foreign.CUDA.Driver.Stream as CUDAS
@@ -127,27 +127,3 @@ createGCF n t2 hsupp_step wstep = do
   where
     wsp = take n $ iterate (\(w, hs) -> (w + wstep, hs + hsupp_step)) (0.0, 0)
     sizeOfGCFInComplexD = sum $ map (\(_, hsupp) -> let supp = 2 * hsupp + 1 in supp * supp * 8 * 8) wsp
-
-main :: IO ()
-main = do
-  CUDA.initialise []
-  dev0 <- CUDA.device 0
-  ctx <- CUDA.create dev0 [CUDA.SchedAuto]
-
-  t0 <- getCurrentTime
-  gcf <- createGCF 31 0.25 4 50.0
-  t1 <- getCurrentTime
-
-  let gsize = gcfSize gcf
-  ptr_host <- CUDA.mallocHostArray [] gsize
-  CUDA.peekArrayAsync gsize (gcfPtr gcf) ptr_host Nothing
-  CUDA.sync
-  BS.unsafePackCStringLen (castPtr (CUDA.useHostPtr ptr_host), gsize * sizeOf (undefined :: CxDouble)) >>= BS.writeFile "GCF.dat"
-  t2 <- getCurrentTime
-  finalizeGCF gcf
-  CUDA.freeHost ptr_host
-
-  print (diffUTCTime t1 t0)
-  print (diffUTCTime t2 t1)
-
-  CUDA.destroy ctx
---
+++ MS3/Sketches/GCF_new/GCFtest.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE
+      CPP
+    , ScopedTypeVariables
+    , FlexibleInstances
+    , TypeSynonymInstances
+  #-}
+
+module Main where
+
+import GCF
+
+import Data.Complex
+import Foreign.Storable.Complex ()
+import Foreign
+import Data.Time.Clock
+import qualified Data.ByteString.Unsafe      as BS
+import qualified Data.ByteString             as BS
+-- import Text.Printf(printf)
+import qualified Foreign.CUDA.Driver as CUDA
+-- import qualified Foreign.CUDA.Driver.Stream as CUDAS
+
+main :: IO ()
+main = do
+  CUDA.initialise []
+  dev0 <- CUDA.device 0
+  ctx <- CUDA.create dev0 [CUDA.SchedAuto]
+
+  t0 <- getCurrentTime
+  gcf <- createGCF 31 0.25 4 50.0
+  t1 <- getCurrentTime
+
+  let gsize = gcfSize gcf
+  ptr_host <- CUDA.mallocHostArray [] gsize
+  CUDA.peekArrayAsync gsize (gcfPtr gcf) ptr_host Nothing
+  CUDA.sync
+  BS.unsafePackCStringLen (castPtr (CUDA.useHostPtr ptr_host), gsize * sizeOf (undefined :: Complex Double)) >>= BS.writeFile "GCF.dat"
+  t2 <- getCurrentTime
+  finalizeGCF gcf
+  CUDA.freeHost ptr_host
+
+  print (diffUTCTime t1 t0)
+  print (diffUTCTime t2 t1)
+
+  CUDA.destroy ctx
