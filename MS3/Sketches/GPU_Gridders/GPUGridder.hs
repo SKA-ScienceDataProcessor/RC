71230ce2d1cf44b8468a4fc64cad896464a2c3fc
Kyrill Briantsev <kyrab@mail.ru>
2015-04-02 14:43:04 +0300

    GPU gridder wrapperis done.

--- MS3/Sketches/GPU_Gridders/GPUGridder.hs
+++ MS3/Sketches/GPU_Gridders/GPUGridder.hs
@@ -8,7 +8,8 @@
 
 module GPUGridder where
 
--- import Foreign.Storable
+import Foreign
+import Foreign.C
 import Foreign.Storable.Complex ()
 import qualified CUDAEx as CUDA
 
@@ -17,7 +18,9 @@ import Data.Binary
 import BinaryInstances ()
 
 import ArgMapper
+import OskarBinReader
 import OskarBinReaderFFI
+import GCF
 
 #ifndef QUICK_TEST
 import Paths_dna_ms3 ( getDataFileName )
@@ -29,6 +32,9 @@ data Grid = Grid {
     gridSize :: !Int
   , gridPtr  :: !CUDA.CxDoubleDevPtr
   } deriving (Generic)
+
+finalizeGrid :: Grid -> IO ()
+finalizeGrid (Grid _ p) = CUDA.free p
 
 instance Binary Grid
 
@@ -42,19 +48,57 @@ instance Binary Grid
  we can launch our kernels in batches with equal max_supp
  thus greatly increasing overall occupation.
  -}
-launchAddBaselines ::
-      CUDA.Fun
-   -> Double
-   -> Int
+
+type At =
+      Int
    -> Int
    -> CUDA.DevicePtr BlWMap
+   -> IO ()
+
+type AddBaselinesFun =
+      CDouble
    -> CUDA.CxDoubleDevPtr
    -> CUDA.DevicePtr CUDA.CxDoubleDevPtr
    -> CUDA.DoubleDevPtr
    -> CUDA.CxDoubleDevPtr
-   -> IO ()
-launchAddBaselines f scale num_of_baselines max_supp permutations gridptr gcflp uvwp visp =
-  CUDA.launchKernel f (num_of_baselines, 1, 1) (nOfThreads, 1, 1) 0 Nothing params
+   -> At
+
+type AddBaselinesIter = Int -> Ptr BlWMap -> At -> IO ()
+
+launchAddBaselines :: CUDA.Fun -> AddBaselinesFun
+launchAddBaselines f scale gridptr gcflp uvwp visp numOfBaselines maxSupp permutations =
+  CUDA.launchKernel f (numOfBaselines, 1, 1) (nOfThreads, 1, 1) 0 Nothing params
   where
-    nOfThreads = min 1024 (max_supp * max_supp)
+    nOfThreads = min 1024 (maxSupp * maxSupp)
     params = mapArgs $ scale :. permutations :. gridptr :. gcflp :. uvwp :. visp :. Z
+
+data GridConfig = GridConfig {
+    gcKernelName :: !String
+  , gcGCFIsFull :: !Bool
+  , gcIter :: !AddBaselinesIter
+  }
+
+runGridder :: GridConfig -> TaskData -> GCF -> IO Grid
+runGridder (GridConfig gfname gcfIsFull iter) td gcf = do
+    fun <- (`CUDA.getFun` gfname) =<< CUDA.loadFile =<< getDataFileName "scatter_gridders_smem_ska.cubin"
+    gridptr <- CUDA.mallocArray gridsize
+    CUDA.memset gridptr (fromIntegral $ gridsize * cxdSize) 0
+    CUDA.allocaArray uvwSize $ \uvwp ->
+      CUDA.allocaArray visSize $ \visp ->
+        CUDA.allocaArray nBaselines $ \permp -> do
+          CUDA.pokeArray uvwSize (castPtr $ tdUVWs td) uvwp
+          CUDA.pokeArray visSize (castPtr $ tdVisibilies td) visp
+          CUDA.pokeArray nBaselines perms permp
+          iter nBaselines perms (launchAddBaselines fun scale visp gcfptr uvwp visp)
+    return $ Grid gridsize gridptr
+  where
+    scale = (2048 - 124 - 1) / (tdMaxx td) -- 124 max hsupp
+    nBaselines = tdBaselines td
+    nPoints = tdPoints td
+    uvwSize = nPoints * 3
+    visSize = nPoints * 4
+    perms = tdMap td
+    gcfptr = if gcfIsFull then getCentreOfFullGCF gcf else gcfLayers gcf
+    gridsize = 4096 * 4096 * 4
+    cxdSize = sizeOf (undefined :: CxDouble)
+    -- permutations gridptr uvwp visp
