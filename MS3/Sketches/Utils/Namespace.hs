24597e787b73ac27f72c8750f103bcc7b7503bba
Kyrill Briantsev <kyrab@mail.ru>
2015-04-09 10:55:45 +0300

    Improve namespace functions.

--- MS3/Sketches/Utils/Namespace.hs
+++ MS3/Sketches/Utils/Namespace.hs
@@ -3,14 +3,16 @@ module Namespace where
 import System.Directory
 import System.FilePath
 
-createNameSpace :: FilePath -> IO ()
+createNameSpace :: FilePath -> IO FilePath
 createNameSpace fp = do
     onWilkes <- doesDirectoryExist "/ramdisks"
     let fn = if onWilkes then ramdir </> n else n
     createDirectory fn
+    return fn
   where
     n = takeBaseName fp
     ramdir = "/ramdisks"
 
-addNameSpace :: FilePath -> FilePath -> IO ()
-addNameSpace outer inner = createDirectoryIfMissing True $ outer </> inner
+addNameSpace :: FilePath -> FilePath -> IO FilePath
+addNameSpace outer inner = createDirectoryIfMissing True d >> return d
+  where d = outer </> inner
