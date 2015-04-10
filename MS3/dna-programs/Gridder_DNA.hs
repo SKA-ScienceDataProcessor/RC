27b6eb5093cea358b459701029be7c9861b4c3d0
Kyrill Briantsev <kyrab@mail.ru>
2015-04-10 20:10:04 +0300

    Save task data on persistent storage.

--- MS3/dna-programs/Gridder_DNA.hs
+++ MS3/dna-programs/Gridder_DNA.hs
@@ -12,11 +12,11 @@ import GridderActors
 
 -- We have only 2 gather gridder variants at the moment and the selection is determined
 --   by how we bin data -- for full or half gcf usage
-runGatherGridderOnSavedData :: Actor (String, String, Bool) ()
-runGatherGridderOnSavedData = actor $ \(ns_in, ns_out, useHalfGcf) -> do
+runGatherGridderOnSavedData :: Actor (String, String, String, Bool) ()
+runGatherGridderOnSavedData = actor $ \(ns_in, ns_tmp, ns_out, useHalfGcf) -> do
   taskData <- eval readTaskDataActor ns_in
-  eval binAndPregridActor (ns_in, useHalfGcf, taskData)
-  runGridderWith (if useHalfGcf then hc else fc) taskData ns_in ns_out
+  eval binAndPregridActor (ns_tmp, useHalfGcf, taskData)
+  runGridderWith (if useHalfGcf then hc else fc) taskData ns_tmp ns_out
   where
      hc = $(mkStaticClosure 'gatherGridderActorHalfGcf)
      fc = $(mkStaticClosure 'gatherGridderActorFullGcf)
@@ -38,13 +38,13 @@ main = do
       shellLoc <- startActor (N 0) $ useLocal >> return $(mkStaticClosure 'runGridderOnLocalData)
       sendParam (ns_loc, taskData, $(mkStaticClosure 'simpleRomeinFullGCF)) shellLoc
       --
-      eval writeTaskDataActor (nst, taskData)
+      eval writeTaskDataActor (ns_loc, taskData)
       --
       shellRG <- startActor (N 1) $ return $(mkStaticClosure 'runGridderOnSavedData)
-      sendParam (nst, ns_rem1, $(mkStaticClosure 'simpleRomeinUsingHalfOfFullGCF)) shellRG
+      sendParam (ns_loc, ns_rem1, $(mkStaticClosure 'simpleRomeinUsingHalfOfFullGCF)) shellRG
       --
       shellGG <- startActor (N 2) $ return $(mkStaticClosure 'runGatherGridderOnSavedData)
-      sendParam (nst, ns_rem2, False) shellGG
+      sendParam (ns_loc, nst, ns_rem2, False) shellGG
       --
       futRG <- delay Remote shellRG
       futGG <- delay Remote shellGG
