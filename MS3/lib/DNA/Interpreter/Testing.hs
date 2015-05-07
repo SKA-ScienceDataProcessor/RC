d9ce111a77175d543390f83ad28b310de663232f
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2015-05-07 17:36:16 +0300

    Add delay before crashing. It uncovers internal error

--- MS3/lib/DNA/Interpreter/Testing.hs
+++ MS3/lib/DNA/Interpreter/Testing.hs
@@ -3,11 +3,17 @@ module DNA.Interpreter.Testing where
 
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Concurrent (threadDelay)
+import Control.Distributed.Process
 import System.Random
+import DNA.Types
 
 -- | Crash process 
-crashMaybe :: MonadIO m => Double ->m ()
-crashMaybe pCrash = liftIO $ do
+crashMaybe :: MonadProcess m => Double -> m ()
+crashMaybe pCrash = do
     roll <- liftIO randomIO
-    when (roll < pCrash) $ error "Ooops crashed"
-    
+    me   <- liftP  getSelfPid
+    when (roll < pCrash) $ do
+        liftIO $ threadDelay (500*1000)
+        liftIO $ putStrLn $ show me ++ " CRASH!"
+        error "Ooops crashed"
