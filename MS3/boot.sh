de6ea9e6902eb1ffb13df2944b2e2ccc6722902e
Peter Wortmann <pw410@cam.ac.uk>
2015-05-12 18:50:31 +0100

    Sandbox creation script
    Might be slightly non-obvious at this point how to put everything
    together...

---
+++ MS3/boot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Set up the sandbox
+cabal sandbox init
+cabal sandbox add-source cupti
+cabal sandbox add-source linux-perf-stat
+
+# Install dependencies into sandbox
+cabal install --only-dependencies
