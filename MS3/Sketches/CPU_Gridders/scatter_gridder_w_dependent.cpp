96c7949ba91f8e09c36726622131dc307be1be3b
Kyrill Briantsev <kyrab@mail.ru>
2015-05-19 20:39:50 +0300

    Fix loops order for CPU gridder (spotted by Peter Wortmann),

--- MS3/Sketches/CPU_Gridders/scatter_gridder_w_dependent.cpp
+++ MS3/Sketches/CPU_Gridders/scatter_gridder_w_dependent.cpp
@@ -101,7 +101,7 @@ void gridKernel_scatter(
       if (use_permutations) bl = permutations[bl0].bl;
       else bl = bl0;
       int max_supp_here = get_supp(permutations[bl].wp);
-      for (int sv = 0; sv < max_supp_here; sv++) { // Moved from 2-levels below according to Romein
+      // for (int sv = 0; sv < max_supp_here; sv++) { // Moved from 2-levels below according to Romein
         for (int i = 0; i < ts_ch; i++) {
             Pregridded p;
             cvt<grid_size, over, w_planes, is_half_gcf, Inp>::pre(scale, wstep, uvw[bl][i], p);
@@ -119,7 +119,7 @@ void gridKernel_scatter(
           for (int su = 0; su < max_supp_here; su++) {
           // NOTE: Romein writes about moving this to the very outer scope
           // (2 levels up) to make things more cache-friendly.
-          // for (int sv = 0; sv < max_supp_here; sv++) {
+          for (int sv = 0; sv < max_supp_here; sv++) {
             // Don't forget our u v are already translated by -max_supp_here/2
             int gsu, gsv;
             gsu = p.u + su;
