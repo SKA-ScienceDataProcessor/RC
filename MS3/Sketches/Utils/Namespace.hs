368f45fae7a66d602cbf12d3ada1831eeda991be
Peter Wortmann <pw410@cam.ac.uk>
2015-05-20 14:16:21 +0100

    Fall back to /tmp for createNameSpace
    Same thing as with file channels - even without ramdisks, this is a good
    bet for a quick-access file system.

--- MS3/Sketches/Utils/Namespace.hs
+++ MS3/Sketches/Utils/Namespace.hs
@@ -1,5 +1,7 @@
 module Namespace where
 
+import Control.Monad ( liftM, filterM )
+
 import System.Directory
 import System.FilePath
 
@@ -11,13 +13,13 @@ isRam _   = False
 
 createNameSpace :: NSType -> FilePath -> IO FilePath
 createNameSpace nstyp fp = do
-    onWilkes <- doesDirectoryExist ramdir
-    let fn = if onWilkes && (isRam nstyp) then ramdir </> n else n
+    let dirs = case nstyp of
+          RAM        -> ["/ramdisks", "/tmp", "."]
+          Persistent -> ["."]
+    dir <- head `liftM` filterM doesDirectoryExist dirs
+    let fn = dir </> takeBaseName fp
     createDirectoryIfMissing True fn
     return fn
-  where
-    n = takeBaseName fp
-    ramdir = "/ramdisks"
 
 addNameSpace :: FilePath -> FilePath -> IO FilePath
 addNameSpace outer inner = createDirectoryIfMissing True d >> return d
