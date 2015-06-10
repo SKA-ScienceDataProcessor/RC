dc7f8f277a938f4417f4b400b67ab46374827259
Kyrill Briantsev <kyrab@mail.ru>
2015-06-10 19:46:00 +0300

    Rearrange posFind code.

--- MS3/Sketches/GCF_new/posFind.cu
+++ MS3/Sketches/GCF_new/posFind.cu
@@ -1,18 +1,5 @@
 #include "threadFenceReduction_kernel.cuh"
-
-struct place {
-  __device__ place & operator=(const place & p) volatile {
-      this->pos = p.pos;
-      this->val = p.val;
-      return const_cast<place&>(*this);
-  }
-  __device__ place (unsigned p, double v) : pos(p), val(v){;}
-  __device__ place (const place & p) : pos(p.pos), val(p.val){;}
-  __device__ place (const volatile place & p) : pos(p.pos), val(p.val){;}
-  
-  size_t pos;
-  double val;
-};
+#include "posFind.cuh"
 
 struct posFinder {
   TASKCFG place init() {return place(0u, 0.0);}
@@ -20,6 +7,6 @@ struct posFinder {
   TASKCFG place f(unsigned int i, double c){return place(i, fabs(c));}
 };
 
-extern "C" __global__ void findPeak_512_e2(const double *g_idata, place *g_odata, unsigned int n) {
+__global__ void findPeak_512_e2(const double *g_idata, place *g_odata, unsigned int n) {
   reduceSinglePass_devGen<512, true, place, double, posFinder>(g_idata, g_odata, n);
 }
---
+++ MS3/Sketches/GCF_new/posFind.cuh
@@ -0,0 +1,15 @@
+struct place {
+  __host__ __device__ place & operator=(const place & p) volatile {
+      this->pos = p.pos;
+      this->val = p.val;
+      return const_cast<place&>(*this);
+  }
+  __host__ __device__ place (unsigned p, double v) : pos(p), val(v){;}
+  __host__ __device__ place (const place & p) : pos(p.pos), val(p.val){;}
+  __host__ __device__ place (const volatile place & p) : pos(p.pos), val(p.val){;}
+  
+  size_t pos;
+  double val;
+};
+
+extern "C" __global__ void findPeak_512_e2(const double *g_idata, place *g_odata, unsigned int n);
