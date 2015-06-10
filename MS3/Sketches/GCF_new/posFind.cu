352826928bf1e9e054e7899269905ae94fdd51ea
Kyrill Briantsev <kyrab@mail.ru>
2015-06-10 19:23:03 +0300

    Improve position finder.

--- MS3/Sketches/GCF_new/posFind.cu
+++ MS3/Sketches/GCF_new/posFind.cu
@@ -6,17 +6,18 @@ struct place {
       this->val = p.val;
       return const_cast<place&>(*this);
   }
-  __device__ place (volatile place & p) : pos(p.pos), val(p.val){;}
   __device__ place (unsigned p, double v) : pos(p), val(v){;}
+  __device__ place (const place & p) : pos(p.pos), val(p.val){;}
+  __device__ place (const volatile place & p) : pos(p.pos), val(p.val){;}
   
   size_t pos;
   double val;
 };
 
 struct posFinder {
-  TASKCFG place init() {return {0u, 0.0};}
+  TASKCFG place init() {return place(0u, 0.0);}
   TASKCFG place reduce(place x, place y){if (y.val > x.val) return y; else return x;}
-  TASKCFG place f(unsigned int i, double c){return {i, fabs(c)};}
+  TASKCFG place f(unsigned int i, double c){return place(i, fabs(c));}
 };
 
 extern "C" __global__ void findPeak_512_e2(const double *g_idata, place *g_odata, unsigned int n) {
