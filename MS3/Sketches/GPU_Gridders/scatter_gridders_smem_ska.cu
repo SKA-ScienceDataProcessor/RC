3fa20876abb047a3dd317a2219f681afddf1fb57
Kyrill Briantsev <kyrab@mail.ru>
2015-06-19 21:39:19 +0300

    Add PSF (IFFT) generation device kernels.

--- MS3/Sketches/GPU_Gridders/scatter_gridders_smem_ska.cu
+++ MS3/Sketches/GPU_Gridders/scatter_gridders_smem_ska.cu
@@ -6,6 +6,19 @@
 #include <math_functions.h>
 
 template <
+    int timesteps
+  , int channels
+  >
+__inline__ __device__ void loadVisIntoSharedMem (
+    const Double4c vis[timesteps * channels]
+  , Double4c vis_shared[timesteps * channels]
+  ) {
+  for (int i = threadIdx.x; i < timesteps * channels; i += blockDim.x) {
+    vis_shared[i] = vis[i];
+  }
+}
+
+template <
     int over
   , int grid_size
 
@@ -13,20 +26,17 @@ template <
   , int channels
   , bool do_mirror
   >
-__inline__ __device__ void loadIntoSharedMem (
+__inline__ __device__ void loadUVWIntoSharedMem (
     double scale
   , double wstep
   , int max_supp // needed for u v translation
   , const double3 uvw[timesteps * channels]
-  , const Double4c vis[timesteps * channels]
   , Pregridded uvo_shared[timesteps * channels]
-  , Double4c vis_shared[timesteps * channels]
   , int2 off_shared[timesteps * channels]
   ) {
   for (int i = threadIdx.x; i < timesteps * channels; i += blockDim.x) {
     // uvo_shared[i] is passed by reference and updated!
     pregridPoint<grid_size, over, do_mirror>(scale, wstep, uvw[i], uvo_shared[i]);
-    vis_shared[i] = vis[i];
     off_shared[i].x = uvo_shared[i].u % max_supp;
     off_shared[i].y = uvo_shared[i].v % max_supp;
   }
@@ -114,6 +124,76 @@ void gridKernel_scatter_kernel_small(
   atomicAdd(&grid[grid_point_u][grid_point_v], sumXX, sumXY, sumYX, sumYY);
 }
 
+////////////////////////////////////
+template <
+    int grid_size
+
+  , int timesteps
+  , int channels
+  , bool is_half_gcf
+  >
+__inline__ __device__
+// psfi must be initialized to 0s.
+void psfiKernel_scatter_kernel_small(
+    int max_supp
+    // For full-size GCF should be passed 0-centered,
+    // i.e. with 0-index in the middle
+  , const complexd * gcf[]
+  , complexd psfi[grid_size][grid_size]
+  , const Pregridded uvo_shared[timesteps * channels]
+  , const int2 off_shared[timesteps * channels]
+  , int myU
+  , int myV
+  ) {
+  complexd sum = {0, 0};
+  int
+      grid_point_u = 0
+    , grid_point_v = 0;
+
+  for (int i = 0; i < timesteps * channels; i++) {
+    int myConvU, myConvV, myGridU, myGridV, u, v;
+    u = uvo_shared[i].u;
+    v = uvo_shared[i].v;
+    myConvU = myU - off_shared[i].x;
+    if (myConvU < 0) myConvU += max_supp;
+    myConvV = myV - off_shared[i].y;
+    if (myConvV < 0) myConvV += max_supp;
+    // This translates points by max_supp/2
+    // returning them back to normal (they were translates by -max_supp/2 before)
+    myGridU = u + myConvU;
+    myGridV = v + myConvV;
+
+    int supp = uvo_shared[i].gcf_layer_supp;
+
+    complexd supportPixel;
+    #define __layeroff myConvU * supp + myConvV
+    if (is_half_gcf) {
+      int index = uvo_shared[i].gcf_layer_index;
+      // Negative index indicates that original w was mirrored
+      // and we shall negate the index to obtain correct
+      // offset *and* conjugate the result.
+      if (index < 0) {
+        supportPixel = gcf[-index][__layeroff];
+        supportPixel.y = - supportPixel.y;
+      } else {
+        supportPixel = gcf[index][__layeroff];
+      }
+    } else {
+        supportPixel = gcf[uvo_shared[i].gcf_layer_index][__layeroff];
+    }
+
+    if (myGridU != grid_point_u || myGridV != grid_point_v) {
+      atomicAdd(&psfi[grid_point_u][grid_point_v], sum);
+      sum = make_cuDoubleComplex(0.0, 0.0);
+      grid_point_u = myGridU;
+      grid_point_v = myGridV;
+    }
+    sum += supportPixel;
+  }
+  atomicAdd(&psfi[grid_point_u][grid_point_v], sum);
+}
+////////////////////////////////////
+
 template <
     int grid_size
 
@@ -147,6 +227,37 @@ void gridKernel_scatter_kernel(
 }
 
 template <
+    int grid_size
+
+  , int timesteps
+  , int channels
+  , bool is_half_gcf
+  >
+__inline__ __device__
+// grid must be initialized to 0s.
+void psfiKernel_scatter_kernel(
+    int max_supp
+  , const complexd * gcf[]
+  , complexd psfi[grid_size][grid_size]
+  , const Pregridded uvo_shared[timesteps * channels]
+  , const int2 off_shared[timesteps * channels]
+  ) {
+  for (int i = threadIdx.x; i < max_supp * max_supp; i += blockDim.x) {
+    int
+        myU = i % max_supp
+      , myV = i / max_supp
+      ;
+    psfiKernel_scatter_kernel_small<
+      grid_size
+
+    , timesteps
+    , channels
+    , is_half_gcf
+    > (max_supp, gcf, psfi, uvo_shared, off_shared, myU, myV);
+  }
+}
+
+template <
     int over
   , int grid_size
 
@@ -167,7 +278,7 @@ __device__ __inline__ void addBaselineToGrid(
   __shared__ Double4c vis_shared[timesteps * channels];
   __shared__ int2 off_shared[timesteps * channels];
   
-  loadIntoSharedMem<
+  loadUVWIntoSharedMem<
       over
     , grid_size
     , timesteps
@@ -177,10 +288,14 @@ __device__ __inline__ void addBaselineToGrid(
     , wstep
     , max_supp
     , uvw
-    , vis
     , uvo_shared
-    , vis_shared
     , off_shared
+    );
+  loadVisIntoSharedMem<
+      timesteps
+    , channels
+    >(vis
+    , vis_shared
     );
   syncthreads();
   gridKernel_scatter_kernel<
@@ -193,6 +308,57 @@ __device__ __inline__ void addBaselineToGrid(
     , grid
     , uvo_shared
     , vis_shared
+    , off_shared
+  );
+}
+
+template <
+    int over
+  , int grid_size
+
+  , int timesteps
+  , int channels
+  , bool is_half_gcf
+  >
+__device__ __inline__ void addBaselineToPsfi(
+    double scale
+  , double wstep
+  , int max_supp
+  , complexd psfi[grid_size][grid_size]
+  , const complexd * gcf[]
+  , const double3 uvw[timesteps * channels]
+  , const Double4c vis[timesteps * channels]
+  ) {
+  // FIXME: should share shared memory with grid computation
+  // Also that perhaps was a bug (we had each instantiation
+  // had it's own shared memory copy).
+  // FIXME: investigate this futher!
+  __shared__ Pregridded uvo_shared[timesteps * channels];
+  __shared__ int2 off_shared[timesteps * channels];
+  
+  loadUVWIntoSharedMem<
+      over
+    , grid_size
+    , timesteps
+    , channels
+    , is_half_gcf
+    >(scale
+    , wstep
+    , max_supp
+    , uvw
+    , uvo_shared
+    , off_shared
+    );
+  syncthreads();
+  psfiKernel_scatter_kernel<
+      grid_size
+    , timesteps
+    , channels
+    , is_half_gcf
+    >(max_supp
+    , gcf
+    , psfi
+    , uvo_shared
     , off_shared
   );
 }
@@ -233,6 +399,42 @@ __device__ __inline__ void addBaselinesToGrid(
     , gcf
     , uvw[bl]
     , vis[bl]
+    );
+}
+
+template <
+    int over
+  , int grid_size
+
+  , int timesteps
+  , int channels
+  , bool is_half_gcf
+  , bool use_permutations
+  >
+__device__ __inline__ void addBaselinesToPsfi(
+    double scale
+  , double wstep
+  , complexd psfi[grid_size][grid_size]
+  , const complexd * gcf[]
+  , const double3 uvw[/* baselines */][timesteps * channels]
+  , int blOff
+  ) {
+  int bl = blockIdx.x + blOff;
+  if (use_permutations) bl = permutations[bl].bl;
+  int max_supp = get_supp(permutations[bl].wp);
+
+  addBaselineToPsfi<
+      over
+    , grid_size
+    , timesteps
+    , channels
+    , is_half_gcf
+    >(scale
+    , wstep
+    , max_supp
+    , psfi
+    , gcf
+    , uvw[bl]
     );
 }
 
