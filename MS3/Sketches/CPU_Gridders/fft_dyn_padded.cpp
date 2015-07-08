58e7e94ff52470aff83049f262b24a9f8b8fedbd
Kyrill Briantsev <kyrab@mail.ru>
2015-07-08 15:25:28 +0300

    CPU 2D FFT for padded data.

---
+++ MS3/Sketches/CPU_Gridders/fft_dyn_padded.cpp
@@ -0,0 +1,49 @@
+#include <fftw3.h>
+#include "common.h"
+
+inline void fft_center(complexd * data, int size, int pitch){
+  bool flip = false;
+  for (int i = 0; i < size * pitch; i+=pitch) {
+    for (int j = 0; j < size; j++) {
+      if (flip) data[i+j] = complexd(-data[i+j].real(), -data[i+j].imag());
+      flip = !flip;
+    }
+  }
+}
+
+// Very simple. Only for even sizes.
+// And for even sizes fftshift and ifftshift coinside.
+inline void fftshift_even(complexd * data, int size, int pitch){
+  complexd tmp;
+  int
+      halfsize = size / 2
+    , maxi = halfsize * pitch
+    ;
+  for (int i = 0; i < maxi; i+=pitch) {
+    for (int j = 0; j < size; j++) {
+       tmp = data[i+maxi+j+halfsize];
+       data[i+maxi+j+halfsize] = data[i+j];
+       data[i+j] = tmp;
+    }
+  }
+}
+
+#define dp reinterpret_cast<fftw_complex*>(data)
+void __fft_inplace(complexd * data, int size, int pitch){
+  int n[] = {size, pitch};
+  fft_center(data, size, pitch);
+  fftw_plan p = fftw_plan_many_dft(
+      2, n, 1
+    , dp, NULL, pitch, 0
+    , dp, NULL, pitch, 0
+    , FFTW_FORWARD, FFTW_ESTIMATE
+    );
+  fftw_execute(p);
+  fftw_destroy_plan(p);
+  fftshift_even(data, size, pitch);
+}
+
+extern "C"
+void fft_inplace_even(complexd * data, int size, int pitch){
+  __fft_inplace(data, size, pitch);
+}
