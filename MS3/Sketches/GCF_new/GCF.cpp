600e9cf8f8cde3e598d66d27a288e989ec3f68e4
Kyrill Briantsev <kyrab@mail.ru>
2015-07-09 14:15:58 +0300

    Finish CPU GCF.

--- MS3/Sketches/GCF_new/GCF.cpp
+++ MS3/Sketches/GCF_new/GCF.cpp
@@ -3,48 +3,6 @@
 
 using namespace std;
 
-extern "C"
-void fft_inplace_even(complexd * data, int size, int pitch);
-
-// In principle, we can calculate 1 - (t2/r)^2 separately
-//   because it does not depend on w and can be reused for
-//   all layers, but we don't bother with caching and copying them
-//   over and thus recalculate them each time
-void mkGCFLayer(
-    complexd arena[] // Full oversampled layer (padded)
-  , int size         // linear size of the arena
-  , int pitch        // pitch of the arena
-  , double t2
-  , double w
-  , int max_support
-  ){
-  acc2d<complexd> center = acc2d<complexd>(arena + size * pitch / 2, pitch);
-
-  int radius = max_support / 2;
-  double normer = t2 / double (radius);
-
-  #pragma omp parallel for // not so much important
-  // Can make this slightly more optimal ...
-  for(int i = 0; i <= radius; i++)
-  for(int j = 0; i <= radius; i++) {
-    double x, y, ph;
-    x = double(i) / normer;
-    y = double(j) / normer;
-    ph = w * (1 - sqrt(1 - x*x - y*y));
-    double s, c;
-    sincos(2.0 * M_PI * ph, &s, &c);
-      center[-i][-j]
-    = center[-i][ j]
-    = center[ i][-j]
-    = center[ i][ j] = complexd(c,-s);
-  }
-  fft_inplace_even(arena, size, pitch);
-  // TODO:
-  // Transposition and extraction here.
-  // I believe we should combine transposition with extraction.
-}
-
-
 template <int over>
 void __transpose_and_extract(
     complexd dst[]       // [over][over][support][support]
@@ -72,14 +30,76 @@ void __transpose_and_extract(
   }
 }
 
+extern "C"
+void fft_inplace_even(complexd * data, int size, int pitch);
+
+// In principle, we can calculate 1 - (t2/r)^2 separately
+//   because it does not depend on w and can be reused for
+//   all layers, but we don't bother with caching and copying them
+//   over and thus recalculate them each time
+template <int over>
+void __mkGCFLayer(
+    complexd dst[]   // [over][over][support][support]
+  , complexd arena[] // Full oversampled layer padded [max_support*over][max_support*over+src_pad]
+  , int support
+  , int max_support
+  , int src_pad      // padding value
+  , double t2
+  , double w
+  ){
+  int
+      size = max_support * over
+    , pitch = size + src_pad
+    ;
+
+  acc2d<complexd> center = acc2d<complexd>(arena + size * pitch / 2, pitch);
+
+  int radius = max_support / 2;
+  double normer = t2 / double (radius);
+
+  #pragma omp parallel for // not so much important
+  // Can make this slightly more optimal ...
+  for(int i = 0; i <= radius; i++)
+  for(int j = 0; i <= radius; i++) {
+    double x, y, ph;
+    x = double(i) / normer;
+    y = double(j) / normer;
+    ph = w * (1 - sqrt(1 - x*x - y*y));
+    double s, c;
+    sincos(2.0 * M_PI * ph, &s, &c);
+      center[-i][-j]
+    = center[-i][ j]
+    = center[ i][-j]
+    = center[ i][ j] = complexd(c,-s);
+  }
+  fft_inplace_even(arena, size, pitch);
+  __transpose_and_extract<over>(
+      dst
+    , arena
+    , support
+    , max_support
+    , src_pad
+    );
+}
+
 // Inst
 extern "C"
-void transpose_and_extract(
-    complexd dst[]
-  , const complexd src[]
+void mkGCFLayer(
+    complexd dst[] // should have [8][8][support][support] size
+  , complexd arena[]
   , int support
   , int max_support
   , int src_pad
-  ) {
-  __transpose_and_extract<8>(dst, src, support, max_support, src_pad);
+  , double t2
+  , double w
+  ){
+  __mkGCFLayer<8>(
+        dst
+      , arena
+      , support
+      , max_support
+      , src_pad
+      , t2
+      , w
+      );
 }
