96d46ab444cd3e6009d27fa4ba9b94c1ea7d8fde
Kyrill Briantsev <kyrab@mail.ru>
2015-07-11 15:20:49 +0300

    Move GPU fftshift fix to MS4.

--- MS4/kernel/gpu/fft/cufftshift.cu
+++ MS4/kernel/gpu/fft/cufftshift.cu
@@ -4,14 +4,14 @@
 // Launch configuration should be as follows:
 //  1. blocks of dim3(threads_per_dim, threads_per_dim, 1) size
 //       where threads_per_dim = min(N, 16)
-//  2. grid of dim3((N+threads_per_dim-1)/threads_per_dim, (N+threads_per_dim-1)/threads_per_dim, 1) blocks
+//  2. grid of dim3((N+threads_per_dim-1)/threads_per_dim, (N-1)/(threads_per_dim * 2)+1, 1) blocks
 
-__device__ __inline__
+static __device__ __inline__
 void fftshift_kernel_common(cuDoubleComplex* data, int shift, int N){
   int x = blockIdx.x * blockDim.x + threadIdx.x;
   int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  if (x > N-1 || y > N-1) return;
+  if (x > N-1 || y > (N+1)/2-1) return;
 
   int x1 = x + shift;
   if (x1 > N-1) x1 -= N;
