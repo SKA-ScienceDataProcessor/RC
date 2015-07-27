0c603c2eb636b74f842deec5191e7b60b9caf8c7
Kyrill Briantsev <kyrab@mail.ru>
2015-07-27 14:14:46 +0300

    Rewrite the binner one more time. We've lost some generality (now assume the data are laid continuously for a single channel) but won being fully compatible with MS4 memory management. And MS4 assumes the single channel for a dataset anyway.

--- MS4/kernel/common/binner.h
+++ MS4/kernel/common/binner.h
@@ -2,20 +2,11 @@
 #define __BINNER_H
 
 #include "common.h"
-#include "metrix.h"
 
-// We 
-typedef struct binTable_tag {
-  void * data;         // this should be freed
-  int dataSizeInBytes; // device allocator must know this
-  int visOffs[DIVIDERS][DIVIDERS];
-  int preOffs[DIVIDERS][DIVIDERS];
-  int nOfItems[DIVIDERS][DIVIDERS];
-} binTable;
-
+// We assume we have the data for a single channel laid continuously in memory!
 extern "C" {
-int bin(binTable tbl[], int num_channels, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size);
-int binm(binTable tbl[], int num_channels, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size);
+int bin(void ** datapp, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size);
+int binm(void ** datapp, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size);
 }
 
 #endif
--- MS4/kernel/cpu/binner/binner.cpp
+++ MS4/kernel/cpu/binner/binner.cpp
@@ -5,6 +5,7 @@
 #define __BINNER
 #define __DYN_GRID_SIZE
 #include "binner.h"
+#include "metrix.h"
 
 #include <cassert>
 #include <cuda.h>
@@ -14,10 +15,20 @@ using namespace std;
 
 typedef unsigned int uint;
 
+struct binTable {
+  int dataSizeInBytes; // device allocator must know this
+  int visOffs[DIVIDERS][DIVIDERS];
+  int preOffs[DIVIDERS][DIVIDERS];
+  int nOfItems[DIVIDERS][DIVIDERS];
+};
+
+// Not sure if I need this alignment but still ...
+const int dataoff = (sizeof(binTable)+31)/32*32;
+
 template <int divs>
 struct streamset {
 
-  int flush(binTable * tp) {
+  int flush(void ** datapp) {
     // CUDA host alloc/free is very expensive, thus we made a single allocation.
     // hence count elements first.
     size_t presiz = 0;
@@ -27,19 +38,21 @@ struct streamset {
     size_t
         num_of_pts = presiz / sizeof(Pregridded)
       , vissiz = num_of_pts * sizeof(complexd)
-      , totsiz = presiz + vissiz;
+      , totsiz = dataoff + vissiz + presiz;
       ;
 
-    int res = cudaHostAlloc(&tp->data, totsiz, 0);
+    int res = cudaHostAlloc(datapp, totsiz, 0);
     if (res != CUDA_SUCCESS) return res;
+
+    binTable *tp = reinterpret_cast<binTable*>(*datapp);
     tp->dataSizeInBytes = totsiz;
-    
+
     int
-        visoff = 0
-      , preoff = vissiz
+        visoff = dataoff
+      , preoff = visoff + vissiz
       ;
-    #define predatap reinterpret_cast<char*>(tp->data)+preoff
-    #define visdatap reinterpret_cast<char*>(tp->data)+visoff
+    #define predatap reinterpret_cast<char*>(*datapp)+preoff
+    #define visdatap reinterpret_cast<char*>(*datapp)+visoff
     for(int i = 0; i < divs; i++) {
       for(int j = 0; j < divs; j++) {
         int plen, n, vlen;
@@ -81,11 +94,11 @@ template <
   >
 // We perform no baselines sorting yet.
 // But it is not so hard to implement it.
-inline int doit(binTable tbl[], int num_channels, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size) {
+// We assume we have the data for a single channel laid continuously in memory!
+inline int doit(void ** datapp, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size) {
   const int div_size = grid_size / divs;
-  int res = 0;
 
-  streamset<divs> * filesp = new streamset<divs>[num_channels];
+  streamset<divs> streams;
 
   complexd
       * amps_end = amps + num_points
@@ -94,89 +107,81 @@ inline int doit(binTable tbl[], int num_channels, int num_points, double scale, 
   Double3 * uvw_curr = uvws;
 
   while(amp_curr < amps_end){
-    for(int i = 0; i < num_channels; i++){
-      Pregridded p;
-      pregridPoint<over, do_mirror>(scale, wstep, *uvw_curr, p); // p is passed as reference and updated!
-      div_t us, vs;
-      us = div(p.u, div_size);
-      vs = div(p.v, div_size);
-      
-      // NOTE: We use the pregridder with no translation, see __BINNER macro defined
-      // in the begginning of this file.
-      // Thus make the translation here.
-      int gcf_layer_supp;
-      gcf_layer_supp = gcfsupps[p.w_plane];
+    Pregridded p;
+    pregridPoint<over, do_mirror>(scale, wstep, *uvw_curr, p); // p is passed as reference and updated!
+    div_t us, vs;
+    us = div(p.u, div_size);
+    vs = div(p.v, div_size);
 
-      // Then finally make the translation
-      // and replace w_plane with gcf_layer_supp
-      p.u += short(grid_size / 2 - gcf_layer_supp / 2);
-      p.v += short(grid_size / 2 - gcf_layer_supp / 2);
-      p.gcf_layer_supp = gcf_layer_supp;
+    // NOTE: We use the pregridder with no translation, see __BINNER macro defined
+    // in the begginning of this file.
+    // Thus make the translation here.
+    int gcf_layer_supp;
+    gcf_layer_supp = gcfsupps[p.w_plane];
 
-      assert(us.quot >= 0 && vs.quot >= 0
-          && us.quot < divs && vs.quot < divs
-          );
+    // Then finally make the translation
+    // and replace w_plane with gcf_layer_supp
+    p.u += short(grid_size / 2 - gcf_layer_supp / 2);
+    p.v += short(grid_size / 2 - gcf_layer_supp / 2);
+    p.gcf_layer_supp = gcf_layer_supp;
 
-      complexd amp_rot;
-      amp_rot = rotw(*amp_curr, uvw_curr->w * scale);
-      filesp[i].put_point(us.quot, vs.quot, p, amp_rot);
+    assert(us.quot >= 0 && vs.quot >= 0
+        && us.quot < divs && vs.quot < divs
+        );
 
-      int margin;
-      margin = gcf_layer_supp / 2;
-      // Optimize slightly for most inhabited w-plane
-      if (margin > 0) {
-        bool leftm, rightm, topm, botm;
-        leftm = false; rightm = false; topm = false; botm = false;
+    complexd amp_rot;
+    amp_rot = rotw(*amp_curr, uvw_curr->w * scale);
+    streams.put_point(us.quot, vs.quot, p, amp_rot);
 
-        if (us.rem < margin && us.quot >= 1) {
-          filesp[i].put_point(us.quot-1, vs.quot, p, amp_rot);
-          leftm = true;
-        }
-        else if (us.rem > div_size - margin && us.quot < divs-1) {
-          filesp[i].put_point(us.quot+1, vs.quot, p, amp_rot);
-          rightm = true;
-        }
+    int margin;
+    margin = gcf_layer_supp / 2;
+    // Optimize slightly for most inhabited w-plane
+    if (margin > 0) {
+      bool leftm, rightm, topm, botm;
+      leftm = false; rightm = false; topm = false; botm = false;
 
-        if (vs.rem < margin && vs.quot >= 1) {
-          filesp[i].put_point(us.quot, vs.quot-1, p, amp_rot);
-          botm = true;
-        }
-        else if (vs.rem > div_size - margin && vs.quot < divs-1) {
-          filesp[i].put_point(us.quot, vs.quot+1, p, amp_rot);
-          topm = true;
-        }
-
-        if (leftm && botm) {
-          filesp[i].put_point(us.quot-1, vs.quot-1, p, amp_rot);
-        }
-        else if (leftm && topm) {
-          filesp[i].put_point(us.quot-1, vs.quot+1, p, amp_rot);
-        }
-        else if (rightm && topm) {
-          filesp[i].put_point(us.quot+1, vs.quot+1, p, amp_rot);
-        }
-        else if (rightm && botm) {
-          filesp[i].put_point(us.quot+1, vs.quot-1, p, amp_rot);
-        }
+      if (us.rem < margin && us.quot >= 1) {
+        streams.put_point(us.quot-1, vs.quot, p, amp_rot);
+        leftm = true;
+      }
+      else if (us.rem > div_size - margin && us.quot < divs-1) {
+        streams.put_point(us.quot+1, vs.quot, p, amp_rot);
+        rightm = true;
       }
 
-      amp_curr++;
-      uvw_curr++;
+      if (vs.rem < margin && vs.quot >= 1) {
+        streams.put_point(us.quot, vs.quot-1, p, amp_rot);
+        botm = true;
+      }
+      else if (vs.rem > div_size - margin && vs.quot < divs-1) {
+        streams.put_point(us.quot, vs.quot+1, p, amp_rot);
+        topm = true;
+      }
+
+      if (leftm && botm) {
+        streams.put_point(us.quot-1, vs.quot-1, p, amp_rot);
+      }
+      else if (leftm && topm) {
+        streams.put_point(us.quot-1, vs.quot+1, p, amp_rot);
+      }
+      else if (rightm && topm) {
+        streams.put_point(us.quot+1, vs.quot+1, p, amp_rot);
+      }
+      else if (rightm && botm) {
+        streams.put_point(us.quot+1, vs.quot-1, p, amp_rot);
+      }
     }
-  }
-  for(int i = 0; i < num_channels; i++){
-    res = filesp[i].flush(tbl+i);
-    if (res != 0) break;
-  }
 
-  delete [] filesp;
-  return res;
+    amp_curr++;
+    uvw_curr++;
+  }
+  return streams.flush(datapp);
 }
 
-int bin(binTable tbl[], int num_channels, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size){
-  return doit<OVER, DIVIDERS, false>(tbl, num_channels, num_points, scale, wstep, amps, uvws, gcfsupps, grid_size);
+int bin(void ** datapp, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size){
+  return doit<OVER, DIVIDERS, false>(datapp, num_points, scale, wstep, amps, uvws, gcfsupps, grid_size);
 }
 
-int binm(binTable tbl[], int num_channels, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size){
-  return doit<OVER, DIVIDERS, true>(tbl, num_channels, num_points, scale, wstep, amps, uvws, gcfsupps, grid_size);
+int binm(void ** datapp, int num_points, double scale, double wstep, complexd* amps, Double3 * uvws, int gcfsupps[], int grid_size){
+  return doit<OVER, DIVIDERS, true>(datapp, num_points, scale, wstep, amps, uvws, gcfsupps, grid_size);
 }
