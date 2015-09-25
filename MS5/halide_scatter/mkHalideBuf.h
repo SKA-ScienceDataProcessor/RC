395b8298ce17e9746a1b5a684984de08cc81a42c
Kyrill Briantsev <kyrab@mail.ru>
2015-09-25 14:59:13 +0300

    Small cleanup.

--- MS5/halide_scatter/mkHalideBuf.h
+++ MS5/halide_scatter/mkHalideBuf.h
@@ -21,7 +21,6 @@ inline
 tuple<buffer_t, buffer_t> mk1DHalideBuf(vector<complexd> & v){
   buffer_t bufr = {0};
   bufr.host = tohh(& reinterpret_cast<double (&)[2]>(v[0])[0]);
-  bufr.min[0]    = 0;
   bufr.stride[0] = 2; // generally nonportable
   bufr.extent[0] = int32_t(v.size());
   bufr.elem_size = sizeof(double);
@@ -36,7 +35,6 @@ inline
 tuple<buffer_t, buffer_t, buffer_t> mk1DHalideBuf(vector<Double3> & v){
   buffer_t bufx = {0};
   bufx.host = tohh(&v[0].u);
-  bufx.min[0]    = 0;
   bufx.stride[0] = 3; // generally nonportable
   bufx.extent[0] = int32_t(v.size());
   bufx.elem_size = sizeof(double);
@@ -54,7 +52,6 @@ template<typename T> inline
 buffer_t mk1DHalideBuf(vector<T> & v){
   buffer_t buf = {0};
   buf.host = tohh(&v[0]);
-  buf.min[0]    = 0;
   buf.stride[0] = 1;
   buf.extent[0] = int32_t(v.size());
   buf.elem_size = sizeof(T);
