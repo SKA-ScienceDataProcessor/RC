db5687c5f644f2d932cc1cee553e92c5b322549a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2015-09-30 13:14:10 +0300

    Add bootstrap script

---
+++ MS5/boot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+# Set up the sandbox
+cabal sandbox init
+cabal sandbox add-source ../MS4/dep/cupti
+cabal sandbox add-source ../MS4/dep/linux-perf-stat
+cabal sandbox add-source ../MS4/dep/oskar
+cabal sandbox add-source dna
+
+# Install dependencies into sandbox
+cabal install --only-dependencies
+
+# Create link to where binaries reside
+ln -s .cabal-sandbox/bin .
