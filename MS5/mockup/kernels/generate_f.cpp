eab2e0fc6fbde71ded2b19fa6f3eb2adafd5bd1e
Peter Wortmann <pw410@cam.ac.uk>
2015-10-01 15:50:03 +0100

    Halide kernel cleanups

--- MS5/mockup/kernels/generate_f.cpp
+++ MS5/mockup/kernels/generate_f.cpp
@@ -11,7 +11,6 @@ int main(int argc, char **argv) {
     gen(x) = cast<float>(x);
 
     std::vector<Argument> args = {};
-    printf("%s\n", argv[1]);
     compile_module_to_object(gen.compile_to_module(args, "kern_generate_f"), argv[1]);
     return 0;
 }
--- MS5/mockup/kernels/generate_g.cpp
+++ MS5/mockup/kernels/generate_g.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv) {
     Func gen;
     Var  x;
 
-    gen(x) = 0.1;
+    gen(x) = 0.1f;
 
     std::vector<Argument> args = {};
     compile_module_to_object(gen.compile_to_module(args, "kern_generate_g"), argv[1]);
