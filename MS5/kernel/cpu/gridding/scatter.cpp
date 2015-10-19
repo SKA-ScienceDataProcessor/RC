5abed4d6beb1a35216fa0fca5bf6a36b5143699c
Peter Wortmann <pw410@cam.ac.uk>
2015-10-19 19:27:00 +0100

    Scatter gridding kernel
    The bindings are wrong at this point, as the grid shouldn't be a parameter,
    but instead simply be passed in as existing data in the output buffer.
    We will have to figure out exactly how to formulate that as a dat
    representation.
    
    But in theory this should be most of the groundwork we need to get a first
    gridder-like pipeline going.

---
+++ MS5/kernel/cpu/gridding/scatter.cpp
@@ -0,0 +1,122 @@
+
+#include "Halide.h"
+
+using namespace Halide;
+
+enum ComplxFields { _REAL = 0, _IMAG,  _CPLX_FIELDS };
+struct Complex {
+  Expr real, imag;
+  Complex(Expr r, Expr i) : real(r), imag(i) {}
+  Complex(FuncRefExpr f) : real(Tuple(f)[0]), imag(Tuple(f)[1]) {}
+  Complex operator+(const Complex &other) const {
+    return{ real + other.real, imag + other.imag };
+  }
+  Complex operator*(const Complex &other) const {
+    return{
+        real * other.real - imag * other.imag
+      , real * other.imag + imag * other.real
+      };
+  }
+  Expr unpack(Expr c) { return select(c == _REAL, real, imag); }
+  operator Tuple() { return Tuple(real, imag); }
+};
+
+int main(int argc, char **argv) {
+  if (argc < 2) return 1;
+
+  // GCF size and oversampling are constants for now
+  const int GCF_SIZE = 16
+          , OVER = 8;
+
+  // ** Input
+
+  Param<double> scale("scale");
+
+  // Visibilities: Array of 5-pairs, packed together with UVW
+  enum VisFields { _R = 0, _C, _U, _V, _W,  _VIS_FIELDS };
+  ImageParam vis(type_of<double>(), 2, "vis");
+  vis.set_min(0,0).set_stride(0,1).set_extent(0,_VIS_FIELDS)
+     .set_stride(1,_VIS_FIELDS);
+  Expr ts_ch = vis.height(); // Number of timesteps
+
+  // GCF: Array of OxOxSxS complex numbers. We "fuse" two dimensions
+  // as Halide only supports up to 4 dimensions.
+  ImageParam gcf_fused(type_of<double>(), 4, "gcf");
+  gcf_fused
+     .set_min(0,0).set_stride(0,1).set_extent(0,_CPLX_FIELDS)
+     .set_min(1,0).set_stride(1,_CPLX_FIELDS).set_extent(1,GCF_SIZE)
+     .set_min(2,0).set_stride(2,_CPLX_FIELDS*GCF_SIZE).set_extent(2,GCF_SIZE)
+     .set_min(3,0).set_stride(3,_CPLX_FIELDS*GCF_SIZE*GCF_SIZE).set_extent(3,OVER*OVER);
+
+  std::vector<Halide::Argument> args = { scale, vis, gcf_fused };
+
+  // ** Output
+
+  // Grid starts out undefined so we can update the output buffer
+  Func uvg("uvg");
+  Var cmplx("cmplx"), x("x"), y("y");
+  uvg(cmplx, x, y) = undef<double>();
+
+  uvg.output_buffer()
+    .set_min(0,0).set_stride(0,1).set_extent(0,_CPLX_FIELDS)
+    .set_min(1,0).set_stride(1,_CPLX_FIELDS);
+  Expr grid_size = uvg.output_buffer().height();
+
+  // ** Helpers
+
+  // Visibility preprocessing
+  Func uvs("uvs"), uv("uv"), overc("overc");
+  Var uvdim("uvdim"), t("t");
+  uvs(uvdim, t) = vis(uvdim, t) * scale;
+  overc(uvdim, t) = clamp(cast<int>(round(OVER * (uvs(uvdim, t) - floor(uvs(uvdim, t))))), 0, OVER-1);
+  uv(uvdim, t) = cast<int>(round(uvs(uvdim, t)) + grid_size / 2 - GCF_SIZE / 2);
+
+  // GCF lookup for a given visibility
+  Func gcf("gcf");
+  Var suppx("suppx"), suppy("suppy"), overx("overx"), overy("overy");
+  gcf(suppx, suppy, t)
+      = Complex(gcf_fused(_REAL, suppx, suppy, overc(_U, t) + OVER * overc(_V, t)),
+                gcf_fused(_IMAG, suppx, suppy, overc(_U, t) + OVER * overc(_V, t)));
+
+  // ** Definition
+
+  // Reduction domain. Note that we iterate over time steps before
+  // switching the GCF row in order to increase locality (Romein).
+  RDom red(
+      0, _CPLX_FIELDS
+    , 0, GCF_SIZE
+    , 0, ts_ch
+    , 0, GCF_SIZE
+    );
+  RVar
+      rcmplx = red.x
+    , rgcfx = red.y
+    , rvis  = red.z
+    , rgcfy = red.w
+    ;
+
+  // Get GCF and visibility as complex numbers
+  Complex visC(vis(_REAL, rvis), vis(_IMAG, rvis));
+  uvg(rcmplx,
+      rgcfx + clamp(uv(_U, rvis), 0, grid_size - 1 - GCF_SIZE),
+      rgcfy + clamp(uv(_V, rvis), 0, grid_size - 1 - GCF_SIZE))
+    += (visC * Complex(gcf(rgcfx, rgcfy, rvis))).unpack(rcmplx);
+
+  // ** Strategy
+
+  // Compute UV & oversampling coordinates per visibility
+  overc.compute_at(uvg, rvis).unroll(uvdim);
+  uv.compute_at(uvg,rvis).unroll(uvdim);
+
+  // Fuse and vectorise complex calculations of entire GCF rows
+  RVar rgcfxc;
+  uvg.update()
+    .allow_race_conditions()
+    .fuse(rgcfx, rcmplx, rgcfxc)
+    .vectorize(rgcfxc, 8)
+    .unroll(rgcfxc, GCF_SIZE * 2 / 8);
+
+  Target target(get_target_from_environment().os, Target::X86, 64, { Target::SSE41, Target::AVX});
+  compile_module_to_object(uvg.compile_to_module(args, "kern_scatter", target), argv[1]);
+  return 0;
+}
--- MS5/programs/Kernel/Gridder.hs
+++ MS5/programs/Kernel/Gridder.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 
 module Kernel.Gridder where
 
@@ -7,11 +8,21 @@ import Flow.Kernel
 
 import Kernel.Data
 
+-- For FFI
+import Data.Vector.HFixed.Class ()
+import Flow.Halide.Types ()
+
 gridInit :: GridPar -> Kernel UVGrid
 gridInit gp = halideKernel0 "gridInit" (uvgRepr gp) undefined
 
+-- | Gridder kernel binding
 gridKernel :: GridPar -> GCFPar -> DomainHandle Range
            -> Flow Vis -> Flow GCFs -> Flow UVGrid
            -> Kernel UVGrid
-gridKernel gp gcfp dh = halideKernel3 "gridKernel" (visRepr dh) (gcfsRepr gcfp) (uvgRepr gp) (uvgRepr gp) undefined
-
+-- TODO: Right now the grid gets copied in, but we would actually like
+-- it to be updated!
+gridKernel gp gcfp dh
+  = halideKernel3 "gridKernel" (visRepr dh) (gcfsRepr gcfp) (uvgRepr gp) (uvgRepr gp) $
+    halideBind kern_scatter (gridTheta gp)
+foreign import ccall unsafe kern_scatter
+  :: HalideBind Double (HalideFun '[VisRepr, GCFsRepr, UVGridRepr] UVGridRepr)
