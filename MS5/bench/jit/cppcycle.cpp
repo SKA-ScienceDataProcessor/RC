567e351b5d44fa1791954248ae08fe4facfab2ad
Peter Wortmann <pw410@cam.ac.uk>
2015-11-19 18:25:39 +0000

    OpenMP JIT benchmark
    This took more fiddling than anticipated - the Halide JIT interface
    is apparently not thread-safe, so we need to bypass it and call the
    kernel directly. This is complicated by the fact that for JIT Halide
    "infers" the argument order and adds an extra "JITUserContext" parameter.
    All this has to match up exactly or the kernel will just crash.
    
    Furthermore, using clock() is unreliable and not very exact. Using
    clock_gettime is to my knowledge the "proper" way for Linux, so let's use
    that instead. Probably not portable, apologies.

--- MS5/bench/jit/cppcycle.cpp
+++ MS5/bench/jit/cppcycle.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <ctime>
 #include <cstring>
+#include <omp.h>
 
 #include "scatter_halide.h"
 #include "../mkHalideBuf.h"
@@ -36,8 +37,29 @@ const int fullSize = gridPitch * gridSize;
 const int numOfVis = num_baselines * num_times;
 const int numOfDoubles = numOfVis * 5;
 
-typedef int (*kern_t)(const double _scale, const int32_t _grid_size, buffer_t *_vis_buffer, buffer_t *_gcf_buffer, buffer_t *_uvg__2_buffer);
-    
+typedef int (*halide_task)(void *user_context, int, uint8_t *);
+
+struct JITHandlers {
+    void (*custom_print)(void *, const char *);
+    void *(*custom_malloc)(void *, size_t);
+    void (*custom_free)(void *, void *);
+    int (*custom_do_task)(void *, halide_task, int, uint8_t *);
+    int (*custom_do_par_for)(void *, halide_task, int, int, uint8_t *);
+    void (*custom_error)(void *, const char *);
+    int32_t (*custom_trace)(void *, const halide_trace_event *);
+    JITHandlers() : custom_print(NULL), custom_malloc(NULL), custom_free(NULL),
+                    custom_do_task(NULL), custom_do_par_for(NULL),
+                    custom_error(NULL), custom_trace(NULL) {
+    }
+};
+
+struct JITUserContext {
+    void *user_context;
+    JITHandlers handlers;
+};
+
+typedef int32_t (*kernfun_t)(buffer_t *, buffer_t *, int32_t, double, JITUserContext *, buffer_t *);
+
 template <typename T>
 void writeImgToDisk(const char * fname, T * out){
   FILE * f = fopen(fname, "wb");
@@ -85,29 +107,38 @@ int readFileToVector(vecd & v, const char * fname){
 
 #define __CK if (res < 0) return res;
 
+int64_t clock_diff(struct timespec *ts) {
+    timespec ts2;
+    clock_gettime(CLOCK_REALTIME, &ts2);
+    return (int64_t)(ts2.tv_sec - ts->tv_sec) * 1000000000 +
+           (int64_t)(ts2.tv_nsec - ts->tv_nsec);
+}
+
 int main(/* int argc, char * argv[] */)
 {
+  // Get number of threads from environment
+  int nthreads;
+  if (getenv("OMP_NUM_THREADS")) {
+      sscanf(getenv("OMP_NUM_THREADS"), "%d", &nthreads);
+  } else {
+      nthreads = 4;
+  }
+  printf("Thread count: %d\n", nthreads);
+  int nchunks = nthreads;
+
+  // Data set-up
   int res;
   vecd vis(numOfDoubles);
   printf("Read visibilities!\n"); fflush(stdout);
-  res = readFileToVector(vis, "vis.dat"); __CK
+  res = readFileToVector(vis, "vis.dat"); __CK;
   vecd gcf(gcfStorageSize * 2); // complex
   printf("Read GCF!\n"); fflush(stdout);
-  res = readFileToVector(gcf, GCF_FILE); __CK
+  res = readFileToVector(gcf, GCF_FILE); __CK;
 
-  vecd uvg(fullSize * 2); // complex
+  // Create a uv-grid for every thread
+  vecd uvg(fullSize * 2 * nthreads); // complex, per thread
 
-  buffer_t
-      vis_buffer = mkHalideBuf<double>(numOfVis,5)
-    , gcf_buffer = mkHalideBuf<double>(over2, gcfSize, gcfSize, 2)
-    , uvg_buffer = mkHalideBuf<double>(gridSize, gridSize, 2)
-    ;
-  vis_buffer.host = tohost(vis.data());
-  gcf_buffer.host = tohost(gcf.data());
-  uvg_buffer.host = tohost(uvg.data());
-
-  Target target_plain(get_target_from_environment().os, Target::X86, 64, { Target::SSE41, Target::AVX});
-
+  // Loop through configurations
   for (int pos = 0; pos < 2; pos++) {
   for (int dim = 0; dim < (1 << _DIM_CONFIGS); dim++) {
   for (int upd = 0; upd <= _UPD_FUSE_UNROLL; upd++) {
@@ -115,6 +146,7 @@ int main(/* int argc, char * argv[] */)
   for (int vector = 2; vector <= 16; vector*=2) {
   if (upd < _UPD_FUSE && vector > 2) break;
 
+  // Create a gridder for the current configuration
   SGridderConfig cfg;
   switch(pos) {
   case 0: cfg.cpos = 0; cfg.xpos = 1; cfg.ypos = 2; cfg.vpos = 3; break;
@@ -123,26 +155,60 @@ int main(/* int argc, char * argv[] */)
   cfg.dim = dim;
   cfg.upd = (UpdConfig) upd;
   cfg.vector = vector;
-  SGridder gridder  = SGridder(cfg);
+  SGridder gridder(cfg);
 
-  gridder.vis.set(Buffer(Float(64), &vis_buffer, "vis"));
-  gridder.gcf_fused.set(Buffer(Float(64), &gcf_buffer, "gcf"));
-  gridder.scale.set(t2);
-  gridder.grid_size.set(gridSize);
+  // Use this to figure out the argument order for the kernel. The
+  // general scheme is:
+  //  1. All input buffers in alphabetical order
+  //  2. All scalar parameters in alphabetical order
+  //  3. The JIT user context
+  //  4. The output buffer(s) in alphabetical order
+  // The call below will give 1+2, but not 3+4!
+  //for( Argument a : gridder.uvg.infer_arguments() ) { printf("%s ", a.name.data()); }
+  //puts("");
 
-  printf("pos %d dim %d upd %d vector %d -", pos, dim, upd, vector);
+  printf("pos %d dim %d upd %d vector %d -", pos, dim, upd, vector); fflush(stdout);
 
-  clock_t ti = clock();
-  gridder.uvg.compile_jit(target_plain);
-  printf("\t%ld\t -", clock() - ti);
+  // Compile. We measure the time, for sanity-checks
+  Target target_plain(get_target_from_environment().os, Target::X86, 64, { Target::SSE41, Target::AVX});
+  timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  kernfun_t kernfun = reinterpret_cast<kernfun_t>(gridder.uvg.compile_jit(target_plain));
+  printf("\t%ld\t -", clock_diff(&ts)); fflush(stdout);
 
   for (int i = 0; i < 4; i++) {
-      memset(uvg.data(), 0, fullSize * 2 * sizeof(double));
-      ti = clock();
-      gridder.uvg.realize(Buffer(Float(64), &uvg_buffer, "uvg"));
-      printf("\t%ld", clock() - ti); fflush(stdout);
+
+    clock_gettime(CLOCK_REALTIME, &ts);
+
+#pragma omp parallel for num_threads(nthreads)
+    for (int chunk = 0; chunk < nchunks; chunk++){
+
+      // Set input and output buffers for Halide. It should only use
+      // an appropriate chunk of visibilities and output to its own grid.
+      int vis0 = chunk * numOfVis / nchunks;
+      int vis1 = (chunk + 1) * numOfVis / nchunks;
+
+      buffer_t
+          vis_buffer = mkHalideBuf<double>(vis1 - vis0,5)
+        , gcf_buffer = mkHalideBuf<double>(over2, gcfSize, gcfSize, 2)
+        , uvg_buffer = mkHalideBuf<double>(gridSize, gridSize, 2)
+        ;
+      vis_buffer.host = tohost(vis.data() + 5 * vis0);
+      vis_buffer.min[1] = vis0;
+      gcf_buffer.host = tohost(gcf.data());
+      uvg_buffer.host = tohost(uvg.data());
+
+      memset(uvg_buffer.host, 0, fullSize * 2 * sizeof(double));
+      JITUserContext jit_context;
+      kernfun(&gcf_buffer, &vis_buffer, gridSize, t2, &jit_context, &uvg_buffer);
+    }
+
+    printf("\t%ld", clock_diff(&ts)); fflush(stdout);
+
   }
+
   puts("");
+
   }}}}
 
   return 0;
