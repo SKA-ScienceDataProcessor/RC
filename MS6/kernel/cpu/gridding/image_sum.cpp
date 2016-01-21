aa105d3084494454c37d72afc6638b38b607d9d0
Peter Wortmann <pw410@cam.ac.uk>
2016-01-21 18:54:38 +0000

    Don't assuming tiling for image sum
    There are no 3rd and 4th dimensions this time around, and trying to
    iterate over them just turns the kernel into a no-op. Oops.

--- MS6/kernel/cpu/gridding/image_sum.cpp
+++ MS6/kernel/cpu/gridding/image_sum.cpp
@@ -5,20 +5,18 @@ using namespace Halide;
 int main(int argc, char **argv) {
     if (argc < 2) return 1;
 
-    ImageParam image(type_of<double>(), 4, "image");
+    ImageParam image(type_of<double>(), 2, "image");
 
     Func sum("sum"); Var x("x"),y("y");
     sum(x,y) = undef<double>();
 
     // Update with data
-    RDom rdom(image.min(0), image.extent(0),
-              image.min(1), image.extent(1),
-              image.min(2), image.extent(2),
-              image.min(3), image.extent(3));
-    RVar rx = rdom.x, ry = rdom.y, rl = rdom.z, rm = rdom.w;
-    sum(clamp(rx+rl, sum.output_buffer().left(), sum.output_buffer().right()),
-        clamp(ry+rm, sum.output_buffer().left(), sum.output_buffer().right()))
-       += image(rx,ry,rl,rm);
+    RDom rdom(print(image.min(0)), print(image.extent(0)),
+              print(image.min(1)), print(image.extent(1)));
+    RVar rx = rdom.x, ry = rdom.y;
+    sum(clamp(rx, sum.output_buffer().left(), sum.output_buffer().right()),
+        clamp(ry, sum.output_buffer().left(), sum.output_buffer().right()))
+       += image(rx,ry);
 
     std::vector<Halide::Argument> args = { image };
     Target target(get_target_from_environment().os, Target::X86, 64, { Target::SSE41, Target::AVX});
