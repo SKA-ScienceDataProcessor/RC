715ac3629830367768419ba4f9c1b4057430ece9
Kyrill Briantsev <kyrab@mail.ru>
2016-02-16 16:57:21 +0300

    Prepare NVidia gridder's Halide veneer to be used as drop-in replacement for the true Halide kernels.

--- MS6/kernel/nvidia/gridder/halide_api.cu
+++ MS6/kernel/nvidia/gridder/halide_api.cu
@@ -3,6 +3,8 @@
 
 extern "C"
 int nvGridder(const double _scale, const int32_t _grid_size, buffer_t *_vis_buffer, buffer_t *_gcf_buffer, buffer_t *_uvg_buffer){
+  // FIXME: make gridGPUx respect buffers locations, i.e.
+  // to check if they already are on GPU and skip allocation and marshaling then
   gridGPUc( _scale
           , reinterpret_cast<CmplxType*>(_uvg_buffer->host)
           , reinterpret_cast<combined*>(_vis_buffer->host)
@@ -10,5 +12,14 @@ int nvGridder(const double _scale, const int32_t _grid_size, buffer_t *_vis_buff
           , _grid_size 
           , reinterpret_cast<CmplxType*>(_gcf_buffer->host)
           );
+  _vis_buffer->dev = 0;
+  _vis_buffer->host_dirty = false;
+  _gcf_buffer->dev = 0;
+  _gcf_buffer->host_dirty = false;
+
+  _uvg_buffer->dev = 0;
+  _uvg_buffer->host_dirty = false;
+  _uvg_buffer->dev_dirty = false;
+
   return 0;
 }
