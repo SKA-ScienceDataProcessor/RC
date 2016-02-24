53ab3e44dd02609a93b9768edf5482aa6286047b
Kyrill Briantsev <kyrab@mail.ru>
2016-02-24 22:59:22 +0300

    More hint utils.

--- MS6/programs/Kernel/Utils.hs
+++ MS6/programs/Kernel/Utils.hs
@@ -5,4 +5,19 @@ import Flow(ProfileHint (..))
 setDblOpts :: Int -> ProfileHint -> ProfileHint
 setDblOpts opts h@FloatHint{} = h {hintDoubleOps = opts}
 setDblOpts opts h@CUDAHint{} = h {hintCudaDoubleOps = opts}
-setDblOpts _ _ = error "Wrong hint for setDblOpts"
+setDblOpts _ ph = ph
+
+-- Perhaps these should be separated ... at least, CUDA stuff ...
+setReadOps :: Int -> ProfileHint -> ProfileHint
+setReadOps n h@MemHint{} = h {hintMemoryReadBytes = n}
+setReadOps n h@IOHint{} = h {hintReadBytes = n}
+setReadOps n h@CUDAHint{} = h {hintCopyBytesDevice = n}
+setReadOps _ ph = ph
+
+setWriteOps :: Int -> ProfileHint -> ProfileHint
+setWriteOps n h@IOHint{} = h {hintWriteBytes = n}
+setWriteOps n h@CUDAHint{} = h {hintCopyBytesHost = n}
+setWriteOps _ ph = ph
+
+setAllOps :: Int -> Int -> Int -> ProfileHint -> ProfileHint
+setAllOps nops nread nwritten = setDblOpts nops . setReadOps nread . setWriteOps nwritten
