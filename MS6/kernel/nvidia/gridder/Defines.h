4e88867c5e5324e1190ec5471ede10657cf6c035
Kyrill Briantsev <kyrab@mail.ru>
2016-02-26 22:28:24 +0300

    Adjust NVidia gridder defs.

--- MS6/kernel/nvidia/gridder/Defines.h
+++ MS6/kernel/nvidia/gridder/Defines.h
@@ -5,10 +5,10 @@
 
 #define NPOINTS (num_baselines*num_times)
 #define GCF_DIM GCF_SIZE
-#define IMG_SIZE 8192
+#define IMG_SIZE 2048
 #define GCF_GRID OVER
 //PTS and GCF_STRIPES affect only GATHER
-#define PTS 2
+#define PTS 1
 #define GCF_STRIPES 4
 #define POLARIZATIONS 1
 //#define DEBUG1
