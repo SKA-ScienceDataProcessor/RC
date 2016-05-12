a5cbb53f0335517e40c7697c96e94e7de76c68ac
thesz <sergueyz@gmail.com>
2016-05-12 21:31:28 +0300

    Fixes for Legion performance tests

--- MS7/libs/scripts/build-legion-tests.sh
+++ MS7/libs/scripts/build-legion-tests.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # This script builds, runs and profiles Legion performance tests and some applications.
 # The only meaningful way to run it is on cluster.
 
 # Verify environment.
 function bad_env {
-    echo "Need SDP_BUILDDIR, SDP_SCRIPTS_DIR and SDP_USE_IBV variables set"
+    echo "Need SDP_BUILDDIR, SDP_SCRIPTS_DIR and SDP_USE_IBV variables set ($1)"
     exit 1
 }
 if [ -z "$SDP_BUILDDIR" ] ; then
-    bad_env
+    bad_env SDP_BUILDDIR
 fi
-if [ -z "$SDP_SCRIPTS_DIR" ] ; then
-    bad_env
+if [ -z "$SDP_SCRIPT_DIR" ] ; then
+    bad_env SDP_SCRIPT_DIR
 fi
 if [ -z "$SDP_USE_IBV" ] ; then
-    bad_env
+    bad_env SDP_USE_IBV
 fi
 
 if [ "$SDP_USE_IBV" != "1" ] ; then
@@ -33,12 +33,18 @@ function build_run_perf_app {
     cd "$performance_tests_dir/$1"
     export EXEC="$2"
     export SRCS="$3"
-    make -f $SDP_SCRIPTS_DIR/cluster_build_rules.mk || exit 1
+    make -f $SDP_SCRIPT_DIR/cluster_build_rules.mk || exit 1
 
     # execute thrice.
-    ./$EXEC
-    ./$EXEC
-    ./$EXEC
+    rm -f execlog
+    echo "one node:" >>execlog
+    ./$EXEC -net=ibv -nodes=1 -tasks=1 -threads=4 >>execlog
+    ./$EXEC -net=ibv -nodes=1 -tasks=1 -threads=4 >>execlog
+    ./$EXEC -net=ibv -nodes=1 -tasks=1 -threads=4 >>execlog
+    echo "two nodes:" >>execlog
+    ./$EXEC -net=ibv -nodes=2 -tasks=2 -threads=4 >>execlog
+    ./$EXEC -net=ibv -nodes=2 -tasks=2 -threads=4 >>execlog
+    ./$EXEC -net=ibv -nodes=2 -tasks=2 -threads=4 >>execlog
 }
 
 # ------------------------------------------------------------------------------
