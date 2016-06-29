e80e254b1ab042f1e8b78d03c88fc33f81c1b9f8
Kyrill Briantsev <kyrab@mail.ru>
2016-06-29 19:31:48 +0300

    Refactor and slightly improve the mapper.

--- MS7/programs/10-Scalability-bi-sectional-bandwidth/report.cc
+++ MS7/programs/10-Scalability-bi-sectional-bandwidth/report.cc
@@ -31,12 +31,14 @@ struct ReportMapper : public DefaultMapper {
   {
     assert(task.is_index_space);
 
-    auto cpus = Machine::ProcessorQuery(machine).only_kind(Processor::LOC_PROC);
-
     if(strcmp(task.get_task_name(), "write")==0) {
         assert(mapper_proc == Processor::NO_PROC);
         mapper_proc = task.current_proc;
         printf("From Mapper! We have the following CPUs:\n");
+#define __GET_CPUS \
+        auto cpus = Machine::ProcessorQuery(machine).only_kind(Processor::LOC_PROC); \
+        size_t middle = cpus.count()/2;
+        __GET_CPUS
         for (auto p : cpus) printf("\t%llx\n", p.id);
         permutation.resize(cpus.count());
         std::copy(cpus.begin(), cpus.end(), permutation.begin());
@@ -45,42 +47,31 @@ struct ReportMapper : public DefaultMapper {
         printf("Permuted to:\n");
         for (auto p : permutation) printf("\t%llx\n", p.id);
 
-        assert(input.domain.get_dim() == 1);
-        auto rect = input.domain.get_rect<1>();
-        size_t middle = cpus.count()/2;
-        assert(rect.dim_size(0) == middle);
-        output.slices.resize(middle);
-        size_t idx = 0;
-        for (LegionRuntime::Arrays::GenericPointInRectIterator<1> pir(rect);
-              pir; pir++, idx++)
-        {
-          Rect<1> slice(pir.p, pir.p);
-          output.slices[idx] = TaskSlice(
-                Domain::from_rect<1>(slice)
-              , permutation[idx]
-              , false
-              , false);
+#define __MK_SLICES(__plus_off)                 \
+        assert(input.domain.get_dim() == 1);    \
+        auto rect = input.domain.get_rect<1>(); \
+        assert(rect.dim_size(0) == middle);     \
+        output.slices.resize(middle);           \
+        size_t idx = 0;                         \
+        for (LegionRuntime::Arrays::GenericPointInRectIterator<1> pir(rect); \
+              pir; pir++, idx++)                \
+        {                                       \
+          Rect<1> slice(pir.p, pir.p);          \
+          output.slices[idx] = TaskSlice(       \
+                Domain::from_rect<1>(slice)     \
+              , permutation[idx __plus_off]     \
+              , false                           \
+              , false);                         \
         }
+
+        __MK_SLICES()
       }
     else if(strcmp(task.get_task_name(), "read")==0) {
         assert(mapper_proc == task.current_proc);
-
-        assert(input.domain.get_dim() == 1);
-        auto rect = input.domain.get_rect<1>();
-        size_t middle = cpus.count()/2;
-        assert(rect.dim_size(0) == middle);
-        output.slices.resize(middle);
-        size_t idx = 0;
-        for (LegionRuntime::Arrays::GenericPointInRectIterator<1> pir(rect);
-              pir; pir++, idx++)
-        {
-          Rect<1> slice(pir.p, pir.p);
-          output.slices[idx] = TaskSlice(
-                Domain::from_rect<1>(slice)
-              , permutation[idx+middle]
-              , false
-              , false);
-        }
+        __GET_CPUS
+#undef __GET_CPUS
+        __MK_SLICES(+middle)
+#undef __MK_SLICES
         // Back to nothing
         mapper_proc = Processor::NO_PROC;
       }
