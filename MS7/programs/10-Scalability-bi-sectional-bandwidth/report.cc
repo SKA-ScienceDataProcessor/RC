15d1ceee13ed3db339834be2ff2c7975f22ac790
Kyrill Briantsev <kyrab@mail.ru>
2016-07-04 08:11:35 +0300

    Bisectional bandwidth: fix mapper -- check which phase we are in and do nothing if we are in the second phase (we've already decided everything in the first stage).

--- MS7/programs/10-Scalability-bi-sectional-bandwidth/report.cc
+++ MS7/programs/10-Scalability-bi-sectional-bandwidth/report.cc
@@ -18,9 +18,11 @@ struct ReportMapper : public DefaultMapper {
   ReportMapper(Machine machine,
       Runtime *rt, Processor local) :
         DefaultMapper(rt->get_mapper_runtime(), machine, local)
+      , local_mapped(false)
       , mapper_proc(Processor::NO_PROC) {}
 
   // We assert both tasks are mapped on the same proc
+  bool local_mapped;
   Processor mapper_proc;
   std::vector<Processor> permutation;
 
@@ -30,15 +32,22 @@ struct ReportMapper : public DefaultMapper {
                                 SliceTaskOutput& output)
   {
     assert(task.is_index_space);
+    size_t middle;
+    Machine::ProcessorQuery cpus(machine);
+#define tname_is(tn) (strcmp(task.get_task_name(),tn)==0)
+    if (!tname_is("write") && !tname_is("read")) goto def;
+    cpus.only_kind(Processor::LOC_PROC);
+    if(task.current_proc != cpus.first() || local_mapped){
+        output.slices.push_back(TaskSlice(input.domain, task.current_proc, false, false));
+        return;
+      }
+    middle = cpus.count()/2;
+    local_mapped = true;
 
-    if(strcmp(task.get_task_name(), "write")==0) {
+    if(tname_is("write")) {
         assert(mapper_proc == Processor::NO_PROC);
         mapper_proc = task.current_proc;
         printf("From Mapper! We have the following CPUs:\n");
-#define __GET_CPUS \
-        auto cpus = Machine::ProcessorQuery(machine).only_kind(Processor::LOC_PROC); \
-        size_t middle = cpus.count()/2;
-        __GET_CPUS
         for (auto p : cpus) printf("\t%llx\n", p.id);
         permutation.resize(cpus.count());
         std::copy(cpus.begin(), cpus.end(), permutation.begin());
@@ -63,23 +72,21 @@ struct ReportMapper : public DefaultMapper {
               , false                           \
               , false);                         \
         }
-
         __MK_SLICES()
       }
-    else if(strcmp(task.get_task_name(), "read")==0) {
+    else if(tname_is("read")) {
         assert(mapper_proc == task.current_proc);
-        __GET_CPUS
-#undef __GET_CPUS
         __MK_SLICES(+middle)
 #undef __MK_SLICES
         // Back to nothing
         mapper_proc = Processor::NO_PROC;
       }
-    else {
-        DefaultMapper::slice_task(ctx, task, input, output);
-        return;
-    }
-  }
+    return;
+
+    def:
+    DefaultMapper::slice_task(ctx, task, input, output);
+  } // slice_task
+
 };
 
 void mapper_registration(Machine machine, Runtime *rt,
